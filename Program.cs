ab54e18a355e0fbb85d0f5f5cea4b1cf6cb24335
cdincer <dincercan@gmail.com>
2020-11-13 11:28:48 +0300

    Initial Project Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpCodeExercises
+{
+    class Program
+    {
+        //My first new project
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
