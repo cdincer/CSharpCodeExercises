eb1c7af67f6e174f7488bdd4ca6d9b7f14828dd3
cdincer <dincercan@gmail.com>
2022-09-19 20:30:35 +0300

    2D Array

--- Tier1/ArrayAndString.cs
+++ Tier1/ArrayAndString.cs
@@ -178,6 +178,146 @@ namespace Tier1
         }
         #endregion
         #endregion
+        #region 2D Arrays
 
+        #region Diagonal Traverse
+        /*
+            Given an m x n matrix mat, return an array of all the elements of the array in a diagonal order.
+         
+            Example 1:
+
+            Input: mat = [[1,2,3],[4,5,6],[7,8,9]]
+            Output: [1,2,4,7,5,3,6,8,9]
+
+            Example 2:
+
+            Input: mat = [[1,2],[3,4]]
+            Output: [1,2,3,4]
+           
+            Constraints:
+
+                m == mat.length
+                n == mat[i].length
+                1 <= m, n <= 104
+                1 <= m * n <= 104
+                -105 <= mat[i][j] <= 105
+
+        https://leetcode.com/problems/diagonal-traverse/solution/
+        */
+        public int[] FindDiagonalOrder(int[][] mat)
+        {
+            // Check for empty matrices
+            if (mat == null || mat.Length == 0)
+            {
+                return new int[0];
+            }
+
+            // Variables to track the size of the matrix
+            int N = mat.Length;
+            int M = mat[0].Length;
+
+            // The two arrays as explained in the algorithm
+            int[] result = new int[N * M];
+            int k = 0;
+            List<int> intermediate = new List<int>();
+
+            // We have to go over all the elements in the first
+            // row and the last column to cover all possible diagonals
+            for (int d = 0; d < N + M - 1; d++)
+            {
+
+                // Clear the intermediate array every time we start
+                // to process another diagonal
+                intermediate.Clear();
+
+                // We need to figure out the "head" of this diagonal
+                // The elements in the first row and the last column
+                // are the respective heads.
+                int r = d < M ? 0 : d - M + 1;
+                int c = d < M ? d : M - 1;
+
+                // Iterate until one of the indices goes out of scope
+                // Take note of the index math to go down the diagonal
+                while (r < N && c > -1)
+                {
+
+                    intermediate.Add(mat[r][c]);
+                    ++r;
+                    --c;
+                }
+
+                // Reverse even numbered diagonals. The
+                // article says we have to reverse odd 
+                // numbered articles but here, the numbering
+                // is starting from 0 :P
+                if (d % 2 == 0)
+                {
+                    intermediate.Reverse();
+                }
+
+                for (int i = 0; i < intermediate.Count; i++)
+                {
+                    result[k++] = intermediate[i];
+                }
+            }
+            return result;
+
+        }
+        public int[] FindDiagonalOrderV2(int[][] mat)
+        {
+            //No jagged arrays,so its fine.
+            int m = mat.Length;
+            int n = mat[0].Length;
+            int row = 0;
+            int col = 0;
+            int idx = 0;
+            bool goingUp = true;
+            int[] result = new int[m * n];
+
+
+            while (idx < m * n)
+            {
+                result[idx++] = mat[row][col];
+
+                if (goingUp)
+                {
+                    row--; col++;
+                }
+                else
+                {
+                    row++; col--;
+                }
+
+                if (row >= m)
+                {
+                    row--;
+                    col += 2;
+                    goingUp ^= true;
+                }
+
+                if (col >= n)
+                {
+                    col--;
+                    row += 2;
+                    goingUp ^= true;
+                }
+
+                if (row < 0)
+                {
+                    row = 0;
+                    goingUp ^= true;
+                }
+
+                if (col < 0)
+                {
+                    col = 0;
+                    goingUp ^= true;
+                }
+            }
+
+            return result;
+        }
+        #endregion
+        #endregion
     }
 }
