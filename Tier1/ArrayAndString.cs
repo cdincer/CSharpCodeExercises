f6d8b0d00a8eacebe2eec51840cd5017aecd2f14
cdincer <dincercan@gmail.com>
2022-10-07 20:26:34 +0300

    Spiral Matrix - 1

--- Tier1/ArrayAndString.cs
+++ Tier1/ArrayAndString.cs
@@ -179,7 +179,6 @@ namespace Tier1
         #endregion
         #endregion
         #region 2D Arrays
-
         #region Diagonal Traverse
         /*
             Given an m x n matrix mat, return an array of all the elements of the array in a diagonal order.
@@ -279,6 +278,53 @@ namespace Tier1
 
         }
         #endregion
+        #region Spiral Matrix 1
+        /*
+        Given an m x n matrix, return all elements of the matrix in spiral order.
+        https://leetcode.com/problems/spiral-matrix/description/
+        */
+        public IList<int> SpiralOrder(int[][] matrix)
+        {
+            List<int> result = new List<int>();
+            if (matrix == null || matrix.Length == 0) return result;
+
+            int startRow = 0, endRow = matrix.Length - 1;
+            int startCol = 0, endCol = matrix[0].Length - 1;
+            int dir = 0;
+
+            while (startRow <= endRow && startCol <= endCol)
+            {
+                switch (dir)
+                {
+                    case 0: //RIGHT
+                        for (int col = startCol; col <= endCol; col++)
+                            result.Add(matrix[startRow][col]);
+                        startRow++;
+                        break;
+                    case 1: //DOWN
+                        for (int row = startRow; row <= endRow; row++)
+                            result.Add(matrix[row][endCol]);
+                        endCol--;
+                        break;
+                    case 2://LEFT
+                        for (int col = endCol; col >= startCol; col--)
+                            result.Add(matrix[endRow][col]);
+                        endRow--;
+                        break;
+                    case 3://UP
+                        for (int row = endRow; row >= startRow; row--)
+                            result.Add(matrix[row][startCol]);
+                        startCol++;
+                        break;
+                }
+                dir = (dir + 1) % 4;
+            }
+            return result;
+        }
         #endregion
+        #endregion
+
+    
+
     }
 }
