cc0b0ef8e2e120f669c5fb4c0bd47f1127e71c8f
cdincer <dincercan@gmail.com>
2022-11-21 22:11:59 +0300

    RotateArray

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace CSharpCodeExercises
             ValidParentheses can1 = new ValidParentheses();
             List<int> items = new List<int>();
             items.ToArray().ToString();
-            newB.MinSubArrayLen(213, new int[] { 12, 28, 83, 4, 25, 26, 25, 2, 25, 25, 25, 12 });
+            newB.Rotate(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3);
             Console.WriteLine("");
         }
     }
