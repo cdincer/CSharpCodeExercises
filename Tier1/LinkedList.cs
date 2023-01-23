4b946133f1bc0deef91dc9fc57b2f449fff211f8
cdincer <dincercan@gmail.com>
2023-01-23 21:05:13 +0300

    Linked List -- Singly Linked List

---
+++ Tier1/LinkedList.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace Tier1
+{
+    public class LinkedList
+    {
+        //Card: https://leetcode.com/explore/learn/card/linked-list/209/singly-linked-list/1287/
+        //1- Add Operation - singly linked list:https://leetcode.com/explore/learn/card/linked-list/209/singly-linked-list/1288/
+        //2- Delete Operation - singly linked list:https://leetcode.com/explore/learn/card/linked-list/209/singly-linked-list/1289/ 
+
+        #region Design Linked List
+        /*
+        https://leetcode.com/problems/design-linked-list/
+        */
+        #endregion
+    }
+}
