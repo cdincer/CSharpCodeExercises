de41cf87f7adde94767d11c8a54fbb4d835a4fbd
cdincer <dincercan@gmail.com>
2023-02-03 23:04:44 +0300

    Two pointer technique Linked List Cycle 1

--- Tier1/LinkedList.cs
+++ Tier1/LinkedList.cs
@@ -8,7 +8,7 @@ namespace Tier1
         //Card: https://leetcode.com/explore/learn/card/linked-list/209/singly-linked-list/1287/
         //1- Add Operation - singly linked list:https://leetcode.com/explore/learn/card/linked-list/209/singly-linked-list/1288/
         //2- Delete Operation - singly linked list:https://leetcode.com/explore/learn/card/linked-list/209/singly-linked-list/1289/ 
-
+        #region Singly Linked List
         #region Design Linked List
         /*
         https://leetcode.com/problems/design-linked-list/
@@ -139,6 +139,100 @@ namespace Tier1
             {
                 val = n;
             }
+        }
+        #endregion
+        #endregion
+        #region Two Pointer Technique
+        //https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1212/
+        /*
+        Explanation for solution below:
+        It is a safe choice to move the slow pointer one step at a time while moving the fast pointer two steps at a time. 
+        For each iteration, the fast pointer will move one extra step. 
+        If the length of the cycle is M, after M iterations, the fast pointer will definitely move one more cycle and catch up with the slow pointer.
+
+        */
+        //Same one in the card: https://leetcode.com/problems/linked-list-cycle/
+        /*
+        Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+        There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
+        Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+        Return true if there is a cycle in the linked list. Otherwise, return false.
+
+        
+
+        Example 1:
+
+        Input: head = [3,2,0,-4], pos = 1
+        Output: true
+        Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+        Example 2:
+
+        Input: head = [1,2], pos = 0
+        Output: true
+        Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+        Example 3:
+
+        Input: head = [1], pos = -1
+        Output: false
+        Explanation: There is no cycle in the linked list.
+
+        
+
+        Constraints:
+
+            The number of the nodes in the list is in the range [0, 104].
+            -105 <= Node.val <= 105
+            pos is -1 or a valid index in the linked-list.
+
+        
+
+        Follow up: Can you solve it using O(1) (i.e. constant) memory?
+
+        */
+
+        /**
+        * Definition for singly-linked list.
+        * public class ListNode {
+        *     public int val;
+        *     public ListNode next;
+        *     public ListNode(int x) {
+        *         val = x;
+        *         next = null;
+        *     }
+        * }
+        */
+         public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int n)
+            {
+                val = n;
+                next = null;
+            }
+        }
+          public bool HasCycle(ListNode head) {
+            if(head == null)
+                return false;
+            
+            ListNode fast=head;
+            ListNode slow=head;
+            
+        while(fast !=null && fast.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+            
+            if(fast == slow)
+            {
+                return true;
+            }
+        }
+        return false;
         }
         #endregion
     }
