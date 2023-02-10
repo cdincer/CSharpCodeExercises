7009ad0dabf23e2b60d0df3f1c5899a4c957551d
cdincer <dincercan@gmail.com>
2023-02-10 20:49:40 +0300

    Intersection of Two Linked Lists

--- Tier1/LinkedList.cs
+++ Tier1/LinkedList.cs
@@ -294,6 +294,112 @@ namespace Tier1
             return null;
         }
         #endregion
+        #region Intersection of Two Linked Lists
+        //https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1215/
+        /*
+        Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+        For example, the following two linked lists begin to intersect at node c1:
+
+        The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+
+        Note that the linked lists must retain their original structure after the function returns.
+
+        Custom Judge:
+
+        The inputs to the judge are given as follows (your program is not given these inputs):
+
+            intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
+            listA - The first linked list.
+            listB - The second linked list.
+            skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
+            skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
+
+        The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program. If you correctly return the intersected node, then your solution will be accepted.
+
+        
+
+        Example 1:
+
+        Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+        Output: Intersected at '8'
+        Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+        From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+        - Note that the intersected node's value is not 1 because the nodes with value 1 in A and B (2nd node in A and 3rd node in B) are different node references. In other words, they point to two different locations in memory, while the nodes with value 8 in A and B (3rd node in A and 4th node in B) point to the same location in memory.
+
+        Example 2:
+
+        Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+        Output: Intersected at '2'
+        Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect).
+        From the head of A, it reads as [1,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected node in A; There are 1 node before the intersected node in B.
+
+        Example 3:
+
+        Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+        Output: No intersection
+        Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
+        Explanation: The two lists do not intersect, so return null.
+
+        https://leetcode.com/problems/intersection-of-two-linked-lists/
+        */
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            ListNode a = headA,
+                  b = headB;
+
+            while (a != b)
+            {
+                a = a == null ? headB : a.next;
+                b = b == null ? headA : b.next;
+            }
+
+            return a;
+
+
+        }
+        #endregion
+        #region Remove Nth Node From End of List
+        /*
+        Given the head of a linked list, remove the nth node from the end of the list and return its head.
+     
+        Example 1:
+
+        Input: head = [1,2,3,4,5], n = 2
+        Output: [1,2,3,5]
+
+        Example 2:
+
+        Input: head = [1], n = 1
+        Output: []
+
+        Example 3:
+
+        Input: head = [1,2], n = 1
+        Output: [1]
+
+        
+
+        Constraints:
+
+            The number of nodes in the list is sz.
+            1 <= sz <= 30
+            0 <= Node.val <= 100
+            1 <= n <= sz
+
+        
+
+        Follow up: Could you do this in one pass?
+        Hint 1: Maintain two pointers and update one with a delay of n steps.
+        Problem: https://leetcode.com/problems/remove-nth-node-from-end-of-list/submissions/895474293/
+        */
+        /*
+        [1]
+        1
+        [1,2]
+        2
+        */
+        #endregion
         #endregion
     }
 }
