dcddbac06912f5b8616e7e5e364d6e62a8d2c19f
cdincer <dincercan@gmail.com>
2023-02-13 20:58:28 +0300

    Remove Nth Node From End of List solution

--- Tier1/LinkedList.cs
+++ Tier1/LinkedList.cs
@@ -399,6 +399,20 @@ namespace Tier1
         [1,2]
         2
         */
+        public ListNode RemoveNthFromEnd(ListNode head, int n) {
+        var left = head;
+        var right = head;
+
+        while(right != null) {
+            right = right.next;
+            if (n-- < 0) left = left.next; 
+        } 
+
+        if (n == 0) head = head.next;
+        else left.next = left.next.next;
+
+        return head;        
+    }
         #endregion
         #endregion
     }
