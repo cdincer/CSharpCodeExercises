26ea60b1a5b60315b3a44421bb451027a12613ce
cdincer <dincercan@gmail.com>
2023-02-17 18:38:01 +0300

    Reverse Linked List - 1

--- Tier1/LinkedList.cs
+++ Tier1/LinkedList.cs
@@ -399,21 +399,68 @@ namespace Tier1
         [1,2]
         2
         */
-        public ListNode RemoveNthFromEnd(ListNode head, int n) {
-        var left = head;
-        var right = head;
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var left = head;
+            var right = head;
 
-        while(right != null) {
-            right = right.next;
-            if (n-- < 0) left = left.next; 
-        } 
+            while (right != null)
+            {
+                right = right.next;
+                if (n-- < 0) left = left.next;
+            }
 
-        if (n == 0) head = head.next;
-        else left.next = left.next.next;
+            if (n == 0) head = head.next;
+            else left.next = left.next.next;
 
-        return head;        
-    }
+            return head;
+        }
         #endregion
+        #endregion
+        #region Classic Problems
+        #region Reverse Linked List
+        /*https://leetcode.com/problems/reverse-linked-list/
+        Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+        Example 1:
+
+        Input: head = [1,2,3,4,5]
+        Output: [5,4,3,2,1]
+
+        Example 2:
+
+        Input: head = [1,2]
+        Output: [2,1]
+
+        Example 3:
+
+        Input: head = []
+        Output: [] 
+
+        Constraints:
+
+            The number of nodes in the list is the range [0, 5000].
+            -5000 <= Node.val <= 5000
+
+
+        Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+        Solution below is inspired by Recursion1 card,Swapchar solution.
+        */
+        public ListNode ReverseList(ListNode head)
+        {
+            return reverseListInt(head, null);
+        }
+
+        private ListNode reverseListInt(ListNode head, ListNode newHead)
+        {
+            if (head == null)
+                return newHead;
+            ListNode next = head.next;
+            head.next = newHead;
+            return reverseListInt(next, head);
+        }
+        #endregion
+
         #endregion
     }
 }
