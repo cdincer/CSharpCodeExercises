99388d59a455043821f4c76a932c5f460d296836
cdincer <dincercan@gmail.com>
2023-03-29 19:56:40 +0200

    Rotate List

--- Tier1/LinkedList.cs
+++ Tier1/LinkedList.cs
@@ -701,8 +701,6 @@ namespace Tier1
 
         You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-        
-
         Example 1:
 
         Input: l1 = [2,4,3], l2 = [5,6,4]
@@ -948,7 +946,50 @@ namespace Tier1
             return head;
         }
         #endregion
+        #region Rotate List
+        /*
+        Given the head of a linked list, rotate the list to the right by k places.
 
+        Example 1:
+
+        Input: head = [1,2,3,4,5], k = 2
+        Output: [4,5,1,2,3]
+
+        Example 2:
+
+        Input: head = [0,1,2], k = 4
+        Output: [2,0,1]
+
+        Constraints:
+
+            The number of nodes in the list is in the range [0, 500].
+            -100 <= Node.val <= 100
+            0 <= k <= 2 * 109
+
+
+        https://leetcode.com/problems/rotate-list/
+        */
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            if (head == null || head.next == null) return head;
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            ListNode fast = dummy, slow = dummy;
+
+            int i;
+            for (i = 0; fast.next != null; i++)//Get the total length 
+                fast = fast.next;
+
+            for (int j = i - k % i; j > 0; j--) //Get the i-n%i th node
+                slow = slow.next;
+
+            fast.next = dummy.next; //Do the rotation
+            dummy.next = slow.next;
+            slow.next = null;
+
+            return dummy.next;
+        }
+        #endregion
         #endregion
     }
 }
