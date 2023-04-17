e3d241d1e4258a0399e57c38caa677e80f9b5ff0
cdincer <dincercan@gmail.com>
2023-04-17 21:08:06 +0200

    Hashtable design array,linkedlist

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using CSharpCodeExercises.Tier1;
 using Leetcode;
 using Tier1;
+using static Tier1.Hashtable;
 using static Tier1.LinkedList;
 
 namespace CSharpCodeExercises
@@ -20,9 +21,16 @@ namespace CSharpCodeExercises
             new int[] {11,12,13,14,15},
             new int[] {16,17,18,19,20},
             };
-            LinkedList test = new LinkedList();
-            ListNode2 head = new ListNode2(1, new ListNode2(2, new ListNode2(3, new ListNode2(4, new ListNode2(5)))));
-            test.RotateRight(head, 2);
+            //ListNode2 head = new ListNode2(1, new ListNode2(2, new ListNode2(3, new ListNode2(4, new ListNode2(5)))));
+            MyHashMap myTestHashMap = new MyHashMap();
+            myTestHashMap.put(Int32.Parse("105"), 1);
+            myTestHashMap.put(Int32.Parse("104"), 2);
+            myTestHashMap.put(Int32.Parse("111"), 3);
+            myTestHashMap.put(Int32.Parse("55"), 22);
+            myTestHashMap.put(Int32.Parse("77"), 22222);
+
+            Console.WriteLine("1111");
+            Console.WriteLine(myTestHashMap.get(int.Parse("55")));
             Console.ReadLine();
         }
     }
