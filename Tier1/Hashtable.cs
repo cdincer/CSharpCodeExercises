c817de329cc0172668e2a498bae4f167f93f2080
cdincer <dincercan@gmail.com>
2023-04-21 15:16:50 +0200

    Contains Duplicate, Single Number

--- Tier1/Hashtable.cs
+++ Tier1/Hashtable.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Tier1
 {   //https://leetcode.com/explore/learn/card/hash-table/
@@ -154,6 +155,112 @@ namespace Tier1
                         }
             }
         }
+        #endregion
+        #region Practical Application - Hash Set
+        #region Contains Duplicate
+        /*
+        Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+        Example 1:
+
+        Input: nums = [1,2,3,1]
+        Output: true
+
+        Example 2:
+
+        Input: nums = [1,2,3,4]
+        Output: false
+
+        Example 3:
+
+        Input: nums = [1,1,1,3,3,4,3,2,4,2]
+        Output: true
+
+        Constraints:
+
+            1 <= nums.length <= 105
+            -109 <= nums[i] <= 109
+
+        https://leetcode.com/problems/contains-duplicate/
+        */
+        //Runtime: 191 ms Beats 92.95 % || Memory Usage: 51.1 MB Beats 99.04 %
+        public bool ContainsDuplicate(int[] nums)
+        {
+
+            HashSet<int> mHash = new HashSet<int>();
+
+            foreach (int number in nums)
+            {
+                if (mHash.Contains(number))
+                {
+                    return true;
+                }
+                else
+                {
+                    mHash.Add(number);
+                }
+            }
+            mHash = null;
+            GC.Collect();
+            return false;
+        }
+        #endregion
+        #region Single Number
+        /*
+        Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+        You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+        
+
+        Example 1:
+
+        Input: nums = [2,2,1]
+        Output: 1
+
+        Example 2:
+
+        Input: nums = [4,1,2,1,2]
+        Output: 4
+
+        Example 3:
+
+        Input: nums = [1]
+        Output: 1
+
+        
+
+        Constraints:
+
+            1 <= nums.length <= 3 * 104
+            -3 * 104 <= nums[i] <= 3 * 104
+            Each element in the array appears twice except for one element which appears only once.
+
+        Before GC.Collect();
+        Runtime: 101 ms
+        Memory Usage: 45.4 MB
+        After GC.Collect();
+        Runtime:104 ms Beats 71.46%
+        Memory:39.7 MB Beats 99.71%
+        https://leetcode.com/problems/single-number/description/
+        */
+        public int SingleNumber(int[] nums)
+        {
+            HashSet<int> counter = new HashSet<int>();
+            foreach (int number in nums)
+            {
+                if (!counter.Contains(number))
+                {
+                    counter.Add(number);
+                }
+                else
+                {
+                    counter.Remove(number);
+                }
+            }
+            return counter.First();
+        }
+        #endregion
         #endregion
         #endregion
     }
