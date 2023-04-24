510ed02aa10f789ebc0b3852542a0a07b929755a
cdincer <dincercan@gmail.com>
2023-04-24 19:45:44 +0200

    Intersection of Two Arrays

--- Tier1/Hashtable.cs
+++ Tier1/Hashtable.cs
@@ -261,6 +261,53 @@ namespace Tier1
             return counter.First();
         }
         #endregion
+        #region Intersection of Two Arrays
+        /*
+        Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+        
+
+        Example 1:
+
+        Input: nums1 = [1,2,2,1], nums2 = [2,2]
+        Output: [2]
+
+        Example 2:
+
+        Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+        Output: [9,4]
+        Explanation: [4,9] is also accepted.
+
+        
+
+        Constraints:
+
+            1 <= nums1.length, nums2.length <= 1000
+            0 <= nums1[i], nums2[i] <= 1000
+
+        https://leetcode.com/problems/intersection-of-two-arrays/description/
+        */
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            HashSet<int> dict = new();
+            HashSet<int> resultDict = new();
+
+            foreach (var num in nums1)
+            {
+                dict.Add(num);
+            }
+
+            foreach (var num in nums2)
+            {
+                if (dict.Contains(num))
+                {
+                    resultDict.Add(num);
+                }
+            }
+
+            return resultDict.ToArray();
+        }
+        #endregion
         #endregion
         #endregion
     }
