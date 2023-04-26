ab918ff92def2abe4285ab8456c822d5e62230b4
cdincer <dincercan@gmail.com>
2023-04-26 19:27:14 +0200

    Happy Number

--- Tier1/Hashtable.cs
+++ Tier1/Hashtable.cs
@@ -308,6 +308,62 @@ namespace Tier1
             return resultDict.ToArray();
         }
         #endregion
+        #region Happy Number
+        /*
+        Write an algorithm to determine if a number n is happy.
+
+        A happy number is a number defined by the following process:
+
+            Starting with any positive integer, replace the number by the sum of the squares of its digits.
+            Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+            Those numbers for which this process ends in 1 are happy.
+
+        Return true if n is a happy number, and false if not.
+        Example 1:
+
+        Input: n = 19
+        Output: true
+        Explanation:
+        12 + 92 = 82
+        82 + 22 = 68
+        62 + 82 = 100
+        12 + 02 + 02 = 1
+
+        Example 2:
+
+        Input: n = 2
+        Output: false
+
+        */
+        public bool IsHappy(int n)
+        {
+            HashSet<int> items = new HashSet<int>();
+            while (n > 0)
+            {
+                int tmp = 0;
+                while (n > 0)
+                {
+                    int i = n % 10;
+                    tmp += i * i;
+                    n = n / 10;
+                }
+                if (tmp == 1)
+                {
+                    return true;
+                }
+                if (!items.Contains(tmp))
+                {
+                    items.Add(tmp);
+                }
+                else
+                {
+                    return false;
+                }
+                n = tmp;
+            }
+            return false;
+        }
+        #endregion
         #endregion
         #endregion
     }
