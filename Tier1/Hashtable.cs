3f55f110b6ab4d28f5b8300c85e87822dd96b25d
cdincer <dincercan@gmail.com>
2023-05-03 19:19:35 +0200

    Hashtable - IsIsomorphic

--- Tier1/Hashtable.cs
+++ Tier1/Hashtable.cs
@@ -417,6 +417,85 @@ namespace Tier1
             return null;
         }
         #endregion
+        #region Isomorphic Strings
+        /*
+        Given two strings s and t, determine if they are isomorphic.
+        Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+        All occurrences of a character must be replaced with another character while preserving the order of characters. 
+        No two characters may map to the same character, but a character may map to itself.
+
+        Example 1:
+
+        Input: s = "egg", t = "add"
+        Output: true
+
+        Example 2:
+
+        Input: s = "foo", t = "bar"
+        Output: false
+
+        Example 3:
+
+        Input: s = "paper", t = "title"
+        Output: true
+
+        Constraints:
+
+            1 <= s.length <= 5 * 104
+            t.length == s.length
+            s and t consist of any valid ascii character.
+
+        https://leetcode.com/problems/isomorphic-strings/description/
+        My Solution below, I have no idea how I came up with this.
+        Test Case:
+        "badc"
+        "baba"
+        Output:false
+        */
+        public bool IsIsomorphic(string s, string t)
+        {
+            Dictionary<char, char> items = new Dictionary<char, char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (items.ContainsKey(s[i]))
+                {
+                    KeyValuePair<char, char> aaa = new KeyValuePair<char, char>(s[i], t[i]);
+
+                    if (!items.Contains(aaa))
+                    {
+                        return false;
+                    }
+
+                }
+                else if (!items.ContainsKey(s[i]))
+                {
+                    items.Add(s[i], t[i]);
+                }
+
+            }
+            items.Clear();
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (items.ContainsKey(t[i]))
+                {
+                    KeyValuePair<char, char> aaa = new KeyValuePair<char, char>(t[i], s[i]);
+
+                    if (!items.Contains(aaa))
+                    {
+                        return false;
+                    }
+
+                }
+                else if (!items.ContainsKey(t[i]))
+                {
+                    items.Add(t[i], s[i]);
+                }
+
+            }
+            return true;
+        }
+        #endregion
         #endregion
 
         #endregion
