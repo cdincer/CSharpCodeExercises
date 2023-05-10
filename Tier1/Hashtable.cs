e2c3cc3afd9fc5b4dc54e789c7717932ac09001c
cdincer <dincercan@gmail.com>
2023-05-10 20:48:10 +0200

    First Unique Character in a String

--- Tier1/Hashtable.cs
+++ Tier1/Hashtable.cs
@@ -571,6 +571,59 @@ namespace Tier1
             return answer.ToArray();
         }
         #endregion
+        #region First Unique Character in a String
+        /*
+        Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+        Example 1:
+        Input: s = "leetcode"
+        Output: 0
+
+        Example 2:
+        Input: s = "loveleetcode"
+        Output: 2
+
+        Example 3:
+        Input: s = "aabb"
+        Output: -1
+
+        Constraints:
+
+            1 <= s.length <= 105
+            s consists of only lowercase English letters.
+
+        https://leetcode.com/problems/first-unique-character-in-a-string/description/
+        Testcase:
+        "aadadaad"
+        */
+        public int FirstUniqChar(string s)
+        {
+            Dictionary<char, int> counter = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!counter.ContainsKey(s[i]))
+                {
+                    counter.Add(s[i], 1);
+                }
+                else
+                {
+                    int itemRecurrence = 0;
+                    counter.TryGetValue(s[i], out itemRecurrence);
+                    counter.Remove(s[i]);
+                    counter.Add(s[i], itemRecurrence + 1);
+                }
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                int itemRecurrence = 0;
+                counter.TryGetValue(s[i], out itemRecurrence);
+                if (itemRecurrence == 1)
+                    return i;
+
+            }
+            return -1;
+        }
+        #endregion
         #endregion
         #endregion
     }
