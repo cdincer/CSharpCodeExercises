d775fc39bfa074a09b5d253ea050e87bbd6a3f63
cdincer <dincercan@gmail.com>
2023-05-17 23:19:26 +0200

    Intersection of Two Arrays II

--- Tier1/Hashtable.cs
+++ Tier1/Hashtable.cs
@@ -265,8 +265,6 @@ namespace Tier1
         /*
         Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
 
-        
-
         Example 1:
 
         Input: nums1 = [1,2,2,1], nums2 = [2,2]
@@ -624,6 +622,66 @@ namespace Tier1
             return -1;
         }
         #endregion
+        #region Intersection of Two Arrays II
+        /*
+        Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+        Example 1:
+
+        Input: nums1 = [1,2,2,1], nums2 = [2,2]
+        Output: [2,2]
+
+        Example 2:
+
+        Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+        Output: [4,9]
+        Explanation: [9,4] is also accepted.
+
+        Constraints:
+
+            1 <= nums1.length, nums2.length <= 1000
+            0 <= nums1[i], nums2[i] <= 1000
+
+        Follow up:
+
+            What if the given array is already sorted? How would you optimize your algorithm?
+            What if nums1's size is small compared to nums2's size? Which algorithm is better?
+            What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+
+
+        https://leetcode.com/problems/intersection-of-two-arrays-ii/
+        */
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            Dictionary<int, int> keeper = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                if (keeper.ContainsKey(nums1[i]))
+                {
+                    keeper[nums1[i]]++;
+                }
+                else
+                {
+                    keeper.Add(nums1[i], 1);
+                }
+            }
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                if (keeper.ContainsKey(nums2[i]))
+                {
+                    keeper[nums2[i]]--;
+                    result.Add(nums2[i]);
+                    if (keeper[nums2[i]] == 0)
+                        keeper.Remove(nums2[i]);
+                }
+            }
+            return result.ToArray();
+        }
+        #endregion
+
         #endregion
         #endregion
     }
