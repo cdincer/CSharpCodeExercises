81617c33f7a69751207de8b032471615230ed813
cdincer <dincercan@gmail.com>
2023-05-19 23:18:29 +0200

    Contains Duplicate II

--- Tier1/Hashtable.cs
+++ Tier1/Hashtable.cs
@@ -681,7 +681,59 @@ namespace Tier1
             return result.ToArray();
         }
         #endregion
+        #region Contains Duplicate II
+        /*
+        Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 
+        Example 1:
+
+        Input: nums = [1,2,3,1], k = 3
+        Output: true
+
+        Example 2:
+
+        Input: nums = [1,0,1,1], k = 1
+        Output: true
+
+        Example 3:
+
+        Input: nums = [1,2,3,1,2,3], k = 2
+        Output: false
+
+        Constraints:
+
+            1 <= nums.length <= 105
+            -109 <= nums[i] <= 109
+            0 <= k <= 105
+        My Solution Below:
+        Runtime: 213 ms(beats 76%)
+        Memory Usage: 59.8 MB(beats 15%)
+        */
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            Dictionary<int, int> keeper = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (keeper.ContainsKey(nums[i]))
+                {
+                    int difference = Math.Abs(keeper.GetValueOrDefault(nums[i]) - i);
+                    if (difference <= k)
+                        return true;
+                    else
+                    {
+                        keeper.Remove(nums[i]);
+                        keeper.Add(nums[i], i);
+                    }
+                }
+                else
+                {
+                    keeper.Add(nums[i], i);
+                }
+            }
+            return false;
+        }
+        #endregion
         #endregion
         #endregion
     }
