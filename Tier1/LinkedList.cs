1a3ad242a2692f42ecac1c6d5798b7e4b575ae8c
cdincer <dincercan@gmail.com>
2023-06-05 14:29:44 +0300

    Find Duplicate Subtrees and Swapping Nodes

--- Tier1/LinkedList.cs
+++ Tier1/LinkedList.cs
@@ -991,5 +991,112 @@ namespace Tier1
         }
         #endregion
         #endregion
+        #region Other LinkedList Related Questions
+        #region Swapping Nodes in a Linked List
+        /*
+        You are given the head of a linked list, and an integer k.
+
+        Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+        Example 1:
+
+        Input: head = [1,2,3,4,5], k = 2
+        Output: [1,4,3,2,5]
+
+        Example 2:
+
+        Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+        Output: [7,9,6,6,8,7,3,0,9,5]
+
+        
+
+        Constraints:
+
+            The number of nodes in the list is n.
+            1 <= k <= n <= 105
+            0 <= Node.val <= 100
+
+
+        https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
+        Test Case:[1,2] k = 1
+        [100,90] k = 2
+        [80,46,66,35,64] k = 1
+        [1] k = 1
+        [100,24,24,36,18,52,95,61,54,88,86,79,11,1,31,26] k = 16
+        Custom Solution Below:
+        Runtime:260 ms Beats 87.51% Memory 60.7 MB Beats 34.70%
+        */
+        public ListNode SwapNodes(ListNode head, int k)
+        {
+            ListNode temp = head;
+
+            int counter = 1;
+            while (head != null)
+            {
+                head = head.next;
+                counter++;
+            }
+
+            //firstValueSearch
+            ListNode firstAttempt = temp;
+            ListNode firstValue = FindNode(firstAttempt, k);
+            int firstCandidate = firstValue.val;
+
+            //secondValueSearch
+            ListNode secondAttempt = temp;
+            ListNode secondValue = FindNode(secondAttempt, counter - k);
+            int secondCandidate = secondValue.val;
+
+            firstValue.val = secondCandidate;
+            secondValue.val = firstCandidate;
+            return temp;
+        }
+
+        public ListNode FindNode(ListNode item, int k)
+        {
+            for (int i = 1; i < k; i++)
+            {
+                item = item.next;
+            }
+            return item;
+        }
+
+        //O(n) time, O(1) space
+        public ListNode SwapNodesV2(ListNode head, int k)
+        {
+            ListNode fast = head;
+            ListNode slow = head;
+            ListNode first = head, second = head;
+
+            // Put fast (k-1) nodes after slow
+            for (int i = 0; i < k - 1; ++i)
+                fast = fast.next;
+
+            // Save the node for swapping
+            first = fast;
+
+            // Move until the end of the list
+            while (fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next;
+            }
+
+            // Save the second node for swapping
+            // Note that the pointer second isn't necessary: we could use slow for swapping as well
+            // However, having second improves readability
+            second = slow;
+
+            // Swap values
+            int temp = first.val;
+            first.val = second.val;
+            second.val = temp;
+
+            return head;
+        }
+        #endregion
+        #endregion
+
+
     }
 }
