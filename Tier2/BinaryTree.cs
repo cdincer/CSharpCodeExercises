054accedad5a761f287958a2c3df0e33aaa0acbc
cdincer <dincercan@gmail.com>
2023-06-23 22:06:18 +0300

    Binary Tree Inorder Traversal

--- Tier2/BinaryTree.cs
+++ Tier2/BinaryTree.cs
@@ -90,6 +90,70 @@ namespace Tier2
             return answer;
         }
         #endregion
-        #endregion 
+        #region Binary Tree Inorder Traversal
+        /*
+        Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+        Example 1:
+
+        Input: root = [1,null,2,3]
+        Output: [1,3,2]
+
+        Example 2:
+
+        Input: root = []
+        Output: []
+
+        Example 3:
+
+        Input: root = [1]
+        Output: [1]
+
+        Constraints:
+
+            The number of nodes in the tree is in the range [0, 100].
+            -100 <= Node.val <= 100
+
+        
+        Follow up: Recursive solution is trivial, could you do it iteratively?
+        https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+        */
+        public List<int> InorderTraversal(TreeNode root)
+        {
+            List<int> res = new();
+            helper(root, res);
+            return res;
+        }
+
+        public void helper(TreeNode root, List<int> res)
+        {
+            if (root != null)
+            {
+                helper(root.left, res);
+                res.Add(root.val);
+                helper(root.right, res);
+            }
+        }
+
+        public List<int> InorderTraversal2(TreeNode root)
+        {
+            List<int> res = new();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            TreeNode curr = root;
+            while (curr != null || stack.Count != 0)
+            {
+                while (curr != null)
+                {
+                    stack.Push(curr);
+                    curr = curr.left;
+                }
+                curr = stack.Pop();
+                res.Add(curr.val);
+                curr = curr.right;
+            }
+            return res;
+        }
+        #endregion
+        #endregion
     }
 }
