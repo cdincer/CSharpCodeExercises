fa93df06615f408c03e2ebf7ec15342552c51a9c
cdincer <dincercan@gmail.com>
2023-07-04 21:26:14 +0300

    Binary Tree Level Order Traversal

--- Tier2/BinaryTree.cs
+++ Tier2/BinaryTree.cs
@@ -223,6 +223,70 @@ namespace Tier2
             return resultStack.ToList();
         }
         #endregion
+        #region Binary Tree Level Order Traversal
+        /*
+        Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+        Example 1:
+
+        Input: root = [3,9,20,null,null,15,7]
+        Output: [[3],[9,20],[15,7]]
+
+        Example 2:
+
+        Input: root = [1]
+        Output: [[1]]
+
+        Example 3:
+
+        Input: root = []
+        Output: []
+
+        Constraints:
+
+            The number of nodes in the tree is in the range [0, 2000].
+            -1000 <= Node.val <= 1000
+
+
+        https://leetcode.com/problems/binary-tree-level-order-traversal/
+        */
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            if (root == null)
+            {
+                return result;
+            }
+
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                int count = queue.Count;
+                List<int> level = new List<int>();
+
+                for (int i = 0; i < count; i++)
+                {
+                    TreeNode cur = queue.Dequeue();
+                    level.Add(cur.val);
+
+                    if (cur.left != null)
+                    {
+                        queue.Enqueue(cur.left);
+                    }
+                    if (cur.right != null)
+                    {
+                        queue.Enqueue(cur.right);
+                    }
+                }
+
+                result.Add(level);
+            }
+
+            return result;
+        }
+        #endregion
         #endregion
     }
 }
