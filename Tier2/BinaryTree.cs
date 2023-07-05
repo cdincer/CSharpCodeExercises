d52167673b246c1975ed89468a39c02c3a55254d
cdincer <dincercan@gmail.com>
2023-07-05 20:36:43 +0300

    Binary Tree GetMaxDepth

--- Tier2/BinaryTree.cs
+++ Tier2/BinaryTree.cs
@@ -287,6 +287,77 @@ namespace Tier2
             return result;
         }
         #endregion
+        #region Maximum Depth of Binary Tree
+        /*
+        Given the root of a binary tree, return its maximum depth.
+
+        A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+        
+
+        Example 1:
+
+        Input: root = [3,9,20,null,null,15,7]
+        Output: 3
+
+        Example 2:
+
+        Input: root = [1,null,2]
+        Output: 2
+
+        
+
+        Constraints:
+
+            The number of nodes in the tree is in the range [0, 104].
+            -100 <= Node.val <= 100
+
+
+        https://leetcode.com/problems/maximum-depth-of-binary-tree/
+        */
+        int maxDepth = 0;
+        public int MaxDepth(TreeNode root)
+        {
+            GetMaxDepth(root, 1);
+            return maxDepth;
+        }
+        private void GetMaxDepth(TreeNode root, int depth)
+        {
+            if (root == null) return;
+
+            if (maxDepth < depth)
+                maxDepth = depth;
+            GetMaxDepth(root.left, depth + 1);
+            GetMaxDepth(root.right, depth + 1);
+        }
+        #endregion
+        #region Symmetric Tree
+        /*
+        Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+        Example 1:
+
+        Input: root = [1,2,2,3,4,4,3]
+        Output: true
+
+        Example 2:
+
+        Input: root = [1,2,2,null,3,null,3]
+        Output: false
+
+        Constraints:
+
+            The number of nodes in the tree is in the range [1, 1000].
+            -100 <= Node.val <= 100
+
+        
+        Follow up: Could you solve it both recursively and iteratively?
+        https://leetcode.com/problems/symmetric-tree/
+        Test Case:Use leetcode tree visualizer to see the differences between trees below.
+        [2,3,3,4,5,5,4,null,null,8,9,null,null,9,8]
+        [2,3,3,5,5,5,5,null,null,8,9,null,null,9,8]
+        [1,2,2,null,3,3]
+        */
+        #endregion
         #endregion
     }
 }
