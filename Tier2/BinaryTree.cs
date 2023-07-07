cd6a546dbf0b02b9b4925db655db8edd4ea88574
cdincer <dincercan@gmail.com>
2023-07-08 00:37:27 +0300

    Binary Tree HasPathSum

--- Tier2/BinaryTree.cs
+++ Tier2/BinaryTree.cs
@@ -441,6 +441,81 @@ namespace Tier2
         #endregion
 
         #endregion
+        #region Path Sum
+        /*
+        Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+        A leaf is a node with no children.
+
+        Example 1:
+
+        Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+        Output: true
+        Explanation: The root-to-leaf path with the target sum is shown.
+
+        Example 2:
+
+        Input: root = [1,2,3], targetSum = 5
+        Output: false
+        Explanation: There two root-to-leaf paths in the tree:
+        (1 --> 2): The sum is 3.
+        (1 --> 3): The sum is 4.
+        There is no root-to-leaf path with sum = 5.
+
+        Example 3:
+
+        Input: root = [], targetSum = 0
+        Output: false
+        Explanation: Since the tree is empty, there are no root-to-leaf paths.
+
+        
+
+        Constraints:
+
+            The number of nodes in the tree is in the range [0, 5000].
+            -1000 <= Node.val <= 1000
+            -1000 <= targetSum <= 1000
+
+
+        https://leetcode.com/problems/path-sum/
+        Test Cases:
+        [1,-2,-3,1,3,-2,null,-1]
+        3
+        []
+        0
+        Custom solution below:
+        Runtime:105 ms Beats:68.75%
+        Memory:42.6 MB Beats:18.90%
+        */
+        private int target = 0;
+        private bool aresult = false;
+        private void pathHelper(TreeNode node, int seperateAmount)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            seperateAmount += node.val;
+            if (node.left == null && node.right == null)
+            {
+                if (target == seperateAmount)
+                {
+                    aresult = true;
+                    return;
+                }
+            }
+            // Visit the root first, then the left subtree, then the right subtree.
+            pathHelper(node.left, seperateAmount);
+            pathHelper(node.right, seperateAmount);
+        }
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            target = targetSum;
+            int random = 0;
+            pathHelper(root, random);
+            return aresult;
+        }
+        #endregion
         #endregion
     }
 }
