51465d0852c890bbdae684d5967b396aeca1dd81
cdincer <dincercan@gmail.com>
2023-07-21 20:49:44 +0300

    Binary Search Tree Iterator

--- Tier2/BinarySearchTree.cs
+++ Tier2/BinarySearchTree.cs
@@ -116,6 +116,114 @@ namespace CSharpCodeExercises.Tier2
             return result;
         }
         #endregion
+        #region Binary Search Tree Iterator
+        /*
+        Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+        BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+        boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+        int next() Moves the pointer to the right, then returns the number at the pointer.
+
+        Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+        You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+        
+
+        Example 1:
+
+        Input
+        ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+        [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+        Output
+        [null, 3, 7, true, 9, true, 15, true, 20, false]
+
+        Explanation
+        BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+        bSTIterator.next();    // return 3
+        bSTIterator.next();    // return 7
+        bSTIterator.hasNext(); // return True
+        bSTIterator.next();    // return 9
+        bSTIterator.hasNext(); // return True
+        bSTIterator.next();    // return 15
+        bSTIterator.hasNext(); // return True
+        bSTIterator.next();    // return 20
+        bSTIterator.hasNext(); // return False
+
+        Constraints:
+
+            The number of nodes in the tree is in the range [1, 105].
+            0 <= Node.val <= 106
+            At most 105 calls will be made to hasNext, and next.
+
+        
+
+         Follow up:
+
+        Could you implement next() and hasNext() to run in average O(1) time and use O(h) memory, where h is the height of the tree?
+        https://leetcode.com/problems/binary-search-tree-iterator/
+        Custom solution below:
+        Runtime: 154 ms Beats:78.28% Memory:55.1 MB Beats:66.67%
+        */
+        /**
+        * Definition for a binary tree node.
+        * public class TreeNode {
+        *     public int val;
+        *     public TreeNode left;
+        *     public TreeNode right;
+        *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+        *         this.val = val;
+        *         this.left = left;
+        *         this.right = right;
+        *     }
+        * }
+        */
+
+
+        public class BSTIterator
+        {
+
+            List<TreeNode> nodesList = new();
+            int IndexTrack = 0;
+            public BSTIterator(TreeNode root)
+            {
+                BuilderTree(root);
+            }
+
+            public int Next()
+            {
+                int value = nodesList[IndexTrack].val;
+                IndexTrack++;
+
+                return value;
+            }
+
+            public bool HasNext()
+            {
+                bool result = true;
+
+                if (IndexTrack >= nodesList.Count())
+                {
+                    return false;
+                }
+
+                if (IndexTrack + 1 < nodesList.Count() && nodesList[IndexTrack + 1] == null)
+                    result = false;
+
+                return result;
+            }
+
+            public void BuilderTree(TreeNode root)
+            {
+                if (root == null)
+                    return;
+
+                BuilderTree(root.left);
+                nodesList.Add(root);
+                BuilderTree(root.right);
+            }
+        }
+        #endregion
         #endregion
     }
 }
