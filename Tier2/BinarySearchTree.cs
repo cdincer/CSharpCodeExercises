9be294d7b69c48589bad8e842b5ac6e6265d7f97
cdincer <dincercan@gmail.com>
2023-07-26 12:53:29 +0300

    Insert into a Binary Search Tree WIP

--- Tier2/BinarySearchTree.cs
+++ Tier2/BinarySearchTree.cs
@@ -259,6 +259,7 @@ namespace CSharpCodeExercises.Tier2
         63
         [4,2,7,1,3]
         5
+       
         */
         public TreeNode SearchBST(TreeNode root, int val)
         {
@@ -277,7 +278,75 @@ namespace CSharpCodeExercises.Tier2
             }
         }
         #endregion
+        #region  Insert into a Binary Search Tree
+        /*
+        You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
 
+        Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+        Example 1:
+
+        Input: root = [4,2,7,1,3], val = 5
+        Output: [4,2,7,1,3,5]
+        Explanation: Another accepted tree is:
+
+        Example 2:
+
+        Input: root = [40,20,60,10,30,50,70], val = 25
+        Output: [40,20,60,10,30,50,70,null,null,25]
+
+        Example 3:
+
+        Input: root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
+        Output: [4,2,7,1,3,5]
+
+        
+
+        Constraints:
+
+            The number of nodes in the tree will be in the range [0, 104].
+            -108 <= Node.val <= 108
+            All the values Node.val are unique.
+            -108 <= val <= 108
+            It's guaranteed that val does not exist in the original BST.
+
+
+        https://leetcode.com/problems/insert-into-a-binary-search-tree/
+        Extra Test Case:
+        [55,28,92,26,43,null,null,null,null,null,null]
+        1
+        */
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            TreeBuilder(root, val);
+            return root;
+        }
+
+        public TreeNode TreeBuilder(TreeNode root, int val)
+        {
+            if (root == null)
+                return null;
+
+            if (val < root.val && root.left != null && root.left.val < val)
+            {
+                return TreeBuilder(root.left, val);
+            }
+            else if (val > root.val && root.right != null && root.right.val > val)
+            {
+                return TreeBuilder(root.right, val);
+            }
+            else if (root.val > val && root.left == null)
+            {
+                return root.left = new TreeNode(val);
+            }
+            else if (root.val < val && root.right == null)
+            {
+                return root.right = new TreeNode(val);
+            }
+
+            return null;
+        }
+        #endregion
         #endregion
     }
 }
