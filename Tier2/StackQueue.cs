849fe6433211abf4fd6ec2d02c73b0f722341671
cdincer <dincercan@gmail.com>
2023-08-18 14:02:14 +0300

    Link/description adjustment

--- Tier2/StackQueue.cs
+++ Tier2/StackQueue.cs
@@ -139,31 +139,10 @@ namespace CSharpCodeExercises.Tier2
 
         https://leetcode.com/problems/number-of-islands/
         */
-        //Template below which is in java supposed to solve this proble.
-        /**
-        * Return the length of the shortest path between root and target node.
-        */
-        // int BFS(Node root, Node target) {
-        //     Queue<Node> queue;  // store all nodes which are waiting to be processed
-        //     int step = 0;       // number of steps neeeded from root to current node
-        //     // initialize
-        //     add root to queue;
-        //     // BFS
-        //     while (queue is not empty) {
-        //         // iterate the nodes which are already in the queue
-        //         int size = queue.size();
-        //         for (int i = 0; i < size; ++i) {
-        //             Node cur = the first node in queue;
-        //             return step if cur is target;
-        //             for (Node next : the neighbors of cur) {
-        //                 add next to queue;
-        //             }
-        //             remove the first node from queue;
-        //         }
-        //         step = step + 1;
-        //     }
-        //     return -1;          // there is no path from root to target
-        // }
+        //Templates in the link below which is in java supposed to solve this problem.
+        //https://leetcode.com/explore/learn/card/queue-stack/231/practical-application-queue/1372/
+
+
         #endregion
         #endregion
         #endregion
