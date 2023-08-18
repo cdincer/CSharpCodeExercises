11a854326c4a8f5d460376e9d85f5f46b68e1b51
cdincer <dincercan@gmail.com>
2023-08-18 13:55:11 +0300

    Number Of Islands description added

--- Tier2/StackQueue.cs
+++ Tier2/StackQueue.cs
@@ -101,6 +101,71 @@ namespace CSharpCodeExercises.Tier2
         #endregion
 
         #endregion
+        #region Queue and BFS
+        #region Number Of Islands
+        /*
+        Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+        An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+        You may assume all four edges of the grid are all surrounded by water.
+
+        Example 1:
+
+        Input: grid = [
+        ["1","1","1","1","0"],
+        ["1","1","0","1","0"],
+        ["1","1","0","0","0"],
+        ["0","0","0","0","0"]
+        ]
+        Output: 1
+
+        Example 2:
+
+        Input: grid = [
+        ["1","1","0","0","0"],
+        ["1","1","0","0","0"],
+        ["0","0","1","0","0"],
+        ["0","0","0","1","1"]
+        ]
+        Output: 3
+
+        Constraints:
+
+            m == grid.length
+            n == grid[i].length
+            1 <= m, n <= 300
+            grid[i][j] is '0' or '1'.
+
+
+        https://leetcode.com/problems/number-of-islands/
+        */
+        //Template below which is in java supposed to solve this proble.
+        /**
+        * Return the length of the shortest path between root and target node.
+        */
+        // int BFS(Node root, Node target) {
+        //     Queue<Node> queue;  // store all nodes which are waiting to be processed
+        //     int step = 0;       // number of steps neeeded from root to current node
+        //     // initialize
+        //     add root to queue;
+        //     // BFS
+        //     while (queue is not empty) {
+        //         // iterate the nodes which are already in the queue
+        //         int size = queue.size();
+        //         for (int i = 0; i < size; ++i) {
+        //             Node cur = the first node in queue;
+        //             return step if cur is target;
+        //             for (Node next : the neighbors of cur) {
+        //                 add next to queue;
+        //             }
+        //             remove the first node from queue;
+        //         }
+        //         step = step + 1;
+        //     }
+        //     return -1;          // there is no path from root to target
+        // }
+        #endregion
+        #endregion
         #endregion
     }
 }
