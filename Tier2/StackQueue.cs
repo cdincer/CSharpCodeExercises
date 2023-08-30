7abc2cf5c142273aa314ce28d05844886c2cbac7
cdincer <dincercan@gmail.com>
2023-08-30 16:05:21 +0300

    Evaluate Reverse Polish Notation

--- Tier2/StackQueue.cs
+++ Tier2/StackQueue.cs
@@ -657,6 +657,113 @@ namespace CSharpCodeExercises.Tier2
             return result;
         }
         #endregion
+        #region Evaluate Reverse Polish Notation
+        /*
+        You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+        Evaluate the expression. Return an integer that represents the value of the expression.
+        Note that:
+
+            The valid operators are '+', '-', '*', and '/'.
+            Each operand may be an integer or another expression.
+            The division between two integers always truncates toward zero.
+            There will not be any division by zero.
+            The input represents a valid arithmetic expression in a reverse polish notation.
+            The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+        Example 1:
+
+        Input: tokens = ["2","1","+","3","*"]
+        Output: 9
+        Explanation: ((2 + 1) * 3) = 9
+
+        Example 2:
+
+        Input: tokens = ["4","13","5","/","+"]
+        Output: 6
+        Explanation: (4 + (13 / 5)) = 6
+
+        Example 3:
+
+        Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+        Output: 22
+        Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+        = ((10 * (6 / (12 * -11))) + 17) + 5
+        = ((10 * (6 / -132)) + 17) + 5
+        = ((10 * 0) + 17) + 5
+        = (0 + 17) + 5
+        = 17 + 5
+        = 22
+
+        
+
+        Constraints:
+
+            1 <= tokens.length <= 104
+            tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+        
+        Test Cases:
+        ["2","1","+","3","*"]
+        ["4","13","5","/","+"]
+        ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+        ["2"]
+        */
+
+        //Custom solution below:
+        /*
+        Stats on 2023-08-30
+        Runtime: 82 ms Beats 77.99% Memory: 40.4 MB Beats:73.48%
+        */
+        public int EvalRPN(string[] tokens)
+        {
+            Stack<int> items = new Stack<int>();
+            int Result = 0;
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/")
+                {
+
+                    int B = 0;
+                    int A = 0;
+                    if (items.Count > 1)
+                    {
+                        B = items.Pop();
+                        A = items.Pop();//Result
+                    }
+                    else
+                    {
+                        B = items.Pop();
+                        A = Result;
+                    }
+
+
+                    switch (tokens[i])
+                    {
+                        case "+":
+                            Result = A + B;
+                            break;
+                        case "-":
+                            Result = A - B;
+                            break;
+                        case "*":
+                            Result = A * B;
+                            break;
+                        case "/":
+                            Result = A / B;
+                            break;
+                        default:
+                            break;
+                    }
+                    items.Push(Result);
+                }
+                else
+                {
+                    items.Push(int.Parse(tokens[i]));
+                }
+            }
+
+            return items.Pop();
+        }
+        #endregion
         #endregion
         #endregion
     }
