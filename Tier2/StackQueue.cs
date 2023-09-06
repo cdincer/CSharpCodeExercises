7851a774b2dacaeedb670aae4e698a6a28ca78f6
cdincer <dincercan@gmail.com>
2023-09-06 19:34:58 +0300

    Target Sum

--- Tier2/StackQueue.cs
+++ Tier2/StackQueue.cs
@@ -951,6 +951,65 @@ namespace CSharpCodeExercises.Tier2
             return resut;
         }
         #endregion
+        #region Target Sum
+        /*
+        You are given an integer array nums and an integer target.
+
+        You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
+
+            For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
+
+        Return the number of different expressions that you can build, which evaluates to target.
+
+        Example 1:
+
+        Input: nums = [1,1,1,1,1], target = 3
+        Output: 5
+        Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+        -1 + 1 + 1 + 1 + 1 = 3
+        +1 - 1 + 1 + 1 + 1 = 3
+        +1 + 1 - 1 + 1 + 1 = 3
+        +1 + 1 + 1 - 1 + 1 = 3
+        +1 + 1 + 1 + 1 - 1 = 3
+
+        Example 2:
+
+        Input: nums = [1], target = 1
+        Output: 1
+
+        Constraints:
+
+            1 <= nums.length <= 20
+            0 <= nums[i] <= 1000
+            0 <= sum(nums[i]) <= 1000
+            -1000 <= target <= 1000
+
+        https://leetcode.com/problems/target-sum/description/
+        */
+        /*Where is the stack for this solution ? Here is their explanation from card for DFS Template 1 in stack section.
+        "It seems like we don't have to use any stacks when we implement DFS recursively. But actually, we are using the implicit stack provided by the system, also known as the Call Stack."
+        **Source: https://leetcode.com/explore/learn/card/queue-stack/232/practical-application-stack/1384/
+        As a result another card without official solution using stack or any official solution at all.
+        */
+        public int FindTargetSumWays(int[] nums, int S)
+        {
+            int count = 0;
+            Dfs(0, 0);
+            return count;
+
+            void Dfs(int currSum, int index)
+            {
+                if (index == nums.Length)
+                {
+                    if (currSum == S)
+                        count++;
+                    return;
+                }
+                Dfs(currSum + nums[index], index + 1);
+                Dfs(currSum - nums[index], index + 1);
+            }
+        }
+        #endregion
         #endregion
         #endregion
     }
