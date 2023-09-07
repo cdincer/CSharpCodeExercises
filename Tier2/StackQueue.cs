37e7de8564caf77e83952c93adb3dd8ec8cab745
cdincer <dincercan@gmail.com>
2023-09-07 17:02:21 +0300

    Binary Tree Inorder Traversal for stack card

--- Tier2/StackQueue.cs
+++ Tier2/StackQueue.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using static Tier2.BinaryTree;
 
 namespace CSharpCodeExercises.Tier2
 {
@@ -1010,6 +1011,54 @@ namespace CSharpCodeExercises.Tier2
             }
         }
         #endregion
+        #region Binary Tree Inorder Traversal
+        /*
+        Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+        Example 1:
+
+        Input: root = [1,null,2,3]
+        Output: [1,3,2]
+
+        Example 2:
+
+        Input: root = []
+        Output: []
+
+        Example 3:
+
+        Input: root = [1]
+        Output: [1]
+
+        Constraints:
+
+            The number of nodes in the tree is in the range [0, 100].
+            -100 <= Node.val <= 100
+
+        Follow up: Recursive solution is trivial, could you do it iteratively?
+        https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+        */
+        //Copy pasted the answer from BinaryTree one, this is how it was done there too.
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            List<int> res = new();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            TreeNode curr = root;
+            while (curr != null || stack.Count != 0)
+            {
+                while (curr != null)
+                {
+                    stack.Push(curr);
+                    curr = curr.left;
+                }
+                curr = stack.Pop();
+                res.Add(curr.val);
+                curr = curr.right;
+            }
+            return res;
+        }
+        #endregion
+
         #endregion
         #endregion
     }
