9a84b0b2df1d21912046cee0a825f5ebd2622c3f
cdincer <dincercan@gmail.com>
2023-09-08 17:38:20 +0300

    Implement Queue using Stacks

--- Tier2/StackQueue.cs
+++ Tier2/StackQueue.cs
@@ -1060,6 +1060,136 @@ namespace CSharpCodeExercises.Tier2
         #endregion
 
         #endregion
+        #region Conclusion
+        #region Implement Queue using Stacks
+        /*
+        Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+        Implement the MyQueue class:
+
+            void push(int x) Pushes element x to the back of the queue.
+            int pop() Removes the element from the front of the queue and returns it.
+            int peek() Returns the element at the front of the queue.
+            boolean empty() Returns true if the queue is empty, false otherwise.
+
+        Notes:
+
+            You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+            Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+
+        Example 1:
+
+        Input
+        ["MyQueue", "push", "push", "peek", "pop", "empty"]
+        [[], [1], [2], [], [], []]
+        Output
+        [null, null, null, 1, 1, false]
+
+        Explanation
+        MyQueue myQueue = new MyQueue();
+        myQueue.push(1); // queue is: [1]
+        myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+        myQueue.peek(); // return 1
+        myQueue.pop(); // return 1, queue is [2]
+        myQueue.empty(); // return false
+
+        Constraints:
+
+            1 <= x <= 9
+            At most 100 calls will be made to push, pop, peek, and empty.
+            All the calls to pop and peek are valid.
+
+        Follow-up: Can you implement the queue such that each operation is amortized O(1) time complexity? In other words, performing n operations will take overall O(n) time even if one of those operations may take longer.
+        https://leetcode.com/problems/implement-queue-using-stacks/description/
+        
+        Test cases for easy copy/paste:
+        ["MyQueue", "push", "push", "peek", "pop", "empty"]
+        [[], [1], [2], [], [], []]
+        ["MyQueue","push","pop","empty"]
+        [[],[1],[],[]]
+        ["MyQueue","push","pop","push","peek"]
+        [[],[1],[],[2],[]]
+        */
+        //Custom solution below crappiest push I have ever written even with that stats are like below. 
+        //It appears this is nearly equal to first editorial solution.
+        //C# Runtime: 96 ms Beats: 87.76% Memory:40.4 MB Beats: 92.95%
+
+        public class MyQueue
+        {
+            public int front { get; set; }
+            public Stack<int> stack1 { get; set; }
+            public Stack<int> stack2 { get; set; }
+            public MyQueue()
+            {
+                stack1 = new Stack<int>();
+                stack2 = new Stack<int>();
+            }
+
+            public void Push(int x)
+            {
+                if (stack2.Count == 0)
+                {
+                    front = x;
+                }
+                stack2.Push(x);
+            }
+            public int Pop()
+            {
+                int newFront = 0;
+                int oldFront = front;
+
+                while (stack2.Count > 0)
+                {
+                    int ItemTobeInserted = stack2.Peek();
+                    if (front != ItemTobeInserted)
+                    {
+                        stack1.Push(stack2.Pop());
+                    }
+                    else
+                    {
+                        stack2.Pop();
+                    }
+                }
+
+                if (stack1.Count > 0)
+                    newFront = stack1.Peek();
+
+                while (stack1.Count > 0)
+                {
+                    int ItemTobeInserted = stack1.Peek();
+
+                    if (front != ItemTobeInserted)
+                    {
+                        stack2.Push(stack1.Pop());
+                    }
+                    else
+                    {
+                        stack1.Pop();
+                    }
+
+                }
+
+                stack1.Clear();
+                front = newFront;
+                return oldFront;
+            }
+
+            public int Peek()
+            {
+                return front;
+            }
+
+            public bool Empty()
+            {
+                if (stack2.Count == 0)
+                    return true;
+
+                return false;
+            }
+        }
+        #endregion
+        #endregion
+
         #endregion
     }
 }
