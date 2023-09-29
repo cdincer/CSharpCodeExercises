a1ef050467280bd6ee92e7e82fd61229e154adea
cdincer <dincercan@gmail.com>
2023-09-29 17:15:33 +0300

    Climbing Stairs

--- Tier2/Recursion1.cs
+++ Tier2/Recursion1.cs
@@ -312,6 +312,58 @@ namespace Tier2
             return result;
         }
         #endregion
+        #region Climbing Stairs
+        /*
+        You are climbing a staircase. It takes n steps to reach the top.
+
+        Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+        Example 1:
+
+        Input: n = 2
+        Output: 2
+        Explanation: There are two ways to climb to the top.
+        1. 1 step + 1 step
+        2. 2 steps
+
+        Example 2:
+
+        Input: n = 3
+        Output: 3
+        Explanation: There are three ways to climb to the top.
+        1. 1 step + 1 step + 1 step
+        2. 1 step + 2 steps
+        3. 2 steps + 1 step
+
+        Constraints:
+            1 <= n <= 45
+
+        https://leetcode.com/problems/climbing-stairs/description/
+        */
+        //No official solution but even the question hint was basically fibonacci number so adapted that.
+        Dictionary<int, int> memValue2 = new();
+        public int ClimbStairs(int n)
+        {
+
+            if (memValue2.ContainsKey(n))
+            {
+                return memValue2[n];
+            }
+            int result = 0;
+
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                result = ClimbStairs(n - 1) + ClimbStairs(n - 2);
+            }
+
+            memValue2.Add(n, result);
+            return memValue2[n];
+        }
+        #endregion
         #endregion
         #endregion
     }
