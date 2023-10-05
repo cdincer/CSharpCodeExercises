15ec6cb95b8554a569f16d3f9eceaab11f36076e
cdincer <dincercan@gmail.com>
2023-10-05 18:46:51 +0300

    K-th Symbol in Grammar WIP

--- Tier1/SlidingWindow.cs
+++ Tier1/SlidingWindow.cs
@@ -67,5 +67,65 @@ namespace Tier1
 
             return length;
         }
+
+
+        #region Maximum Subarray
+        /*
+        Given an integer array nums, find the
+        subarray
+        with the largest sum, and return its sum.
+
+        Example 1:
+        Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+        Output: 6
+        Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+        Example 2:
+        Input: nums = [1]
+        Output: 1
+        Explanation: The subarray [1] has the largest sum 1.
+
+        Example 3:
+        Input: nums = [5,4,-1,7,8]
+        Output: 23
+        Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+        Constraints:
+
+            1 <= nums.length <= 105
+            -104 <= nums[i] <= 104
+
+        
+
+        Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+        https://leetcode.com/problems/maximum-subarray/
+        Extra Test Case:
+        [-1]
+        205th case is in submission to big to copy here.
+        */
+        public int MaxSubArray(int[] nums)
+        {
+            int currMax = 0;
+            int ourMax = int.MinValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                currMax = currMax + nums[i];
+                if (currMax > ourMax)
+                {
+                    ourMax = currMax;
+                }
+
+                if (currMax < 0)
+                {
+                    currMax = 0;
+                }
+            }
+
+            return ourMax;
+        }
+        #endregion
+
     }
 }
--- Tier2/Recursion1.cs
+++ Tier2/Recursion1.cs
@@ -557,6 +557,33 @@ namespace Tier2
         
         https://leetcode.com/problems/k-th-symbol-in-grammar/
         */
+        public int depthFirstSearch(int n, int k, int rootVal)
+        {
+            if (n == 1)
+            {
+                return rootVal;
+            }
+
+            int totalNodes = (int)Math.Pow(2, n - 1);
+
+            // Target node will be present in the right half sub-tree of the current root node.
+            if (k > (totalNodes / 2))
+            {
+                int nextRootVal = (rootVal == 0) ? 1 : 0;
+                return depthFirstSearch(n - 1, k - (totalNodes / 2), nextRootVal);
+            }
+            // Otherwise, the target node is in the left sub-tree of the current root node.
+            else
+            {
+                int nextRootVal = (rootVal == 0) ? 0 : 1;
+                return depthFirstSearch(n - 1, k, nextRootVal);
+            }
+        }
+
+        public int KthGrammar(int n, int k)
+        {
+            return depthFirstSearch(n, k, 0);
+        }
         #endregion
         #endregion
         #endregion
