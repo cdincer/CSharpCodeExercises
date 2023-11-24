eb48f4f8ebbbfbeddedc7d9d2a965a84e131f4ec
cdincer <dincercan@gmail.com>
2023-11-24 19:09:59 +0300

    Evaluate Division WIP

--- Tier3/Graph.cs
+++ Tier3/Graph.cs
@@ -199,8 +199,108 @@ namespace CSharpCodeExercises.Tier3
         #endregion
         #region Evaluate Division
         /* 
+        You are given an array of variable pairs equations and an array of real numbers values, 
+        where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i]. 
+        Each Ai or Bi is a string that represents a single variable.
+        You are also given some queries, where queries[j] = [Cj, Dj] represents the jth query where you must find the answer for Cj / Dj = ?.
+        Return the answers to all queries. If a single answer cannot be determined, return -1.0.
+        Note: The input is always valid. You may assume that evaluating the queries will not result in division by zero and that there is no contradiction.
+        Note: The variables that do not occur in the list of equations are undefined, so the answer cannot be determined for them.
+
+        Example 1:
+        Input: 
+        equations = [["a","b"],["b","c"]], 
+        values = [2.0,3.0], 
+        queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+        Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+        Explanation: 
+        Given: a / b = 2.0, b / c = 3.0
+        queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? 
+        return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
+        note: x is undefined => -1.0
+
+        Example 2:
+        Input: 
+        equations = [["a","b"],["b","c"],["bc","cd"]], 
+        values = [1.5,2.5,5.0], 
+        queries = [["a","c"],["c","b"],["bc","cd"],["cd","bc"]]
+        Output: [3.75000,0.40000,5.00000,0.20000]
+
+        Example 3:
+        Input: 
+        equations = [["a","b"]], 
+        values = [0.5],
+        queries = [["a","b"],["b","a"],["a","c"],["x","y"]]
+        Output: [0.50000,2.00000,-1.00000,-1.00000]
+
         https://leetcode.com/problems/evaluate-division/
         */
+        /* 
+        C# Test Case Prep 1st Scenario:
+        List<IList<string>> equations = new();
+        equations.Add(new List<string>() {"a","b"});
+        equations.Add(new List<string>() {"b","c" });
+        double[] values = new [] {2.0,3.0};
+        List<IList<string>> queries = new();
+        queries.Add(new List<string>() {"a","c" });
+        queries.Add(new List<string>() {"b","a" });
+        queries.Add(new List<string>() {"a","e" });
+        queries.Add(new List<string>() {"a","a" });
+        queries.Add(new List<string>() {"x","x" });
+        */
+        public double[] CalcEquation(IList<IList<string>> equations, double[] values, IList<IList<string>> queries)
+        {
+            double[] results = new double[queries.Count];
+            Dictionary<string, List<Tuple<string, double>>> dict = new Dictionary<string, List<Tuple<string, double>>>();
+            HashSet<string> pairs = new HashSet<string>();
+
+            for (int i = 0; i < equations.Count; i++)
+            {
+                if (!dict.ContainsKey(equations[i][0]))
+                    dict.Add(equations[i][0], new List<Tuple<string, double>>());
+
+                dict[equations[i][0]].Add(new Tuple<string, double>(equations[i][1], values[i]));
+
+                if (!dict.ContainsKey(equations[i][1]))
+                    dict.Add(equations[i][1], new List<Tuple<string, double>>());
+
+                dict[equations[i][1]].Add(new Tuple<string, double>(equations[i][0], 1 / values[i]));
+
+                if (!pairs.Contains(equations[i][0]))
+                    pairs.Add(equations[i][0]);
+
+                if (!pairs.Contains(equations[i][1]))
+                    pairs.Add(equations[i][1]);
+            }
+
+            for (int i = 0; i < queries.Count; i++)
+                if (pairs.Contains(queries[i][0]) && pairs.Contains(queries[i][1]))
+                    results[i] = DFS(dict, new HashSet<string>(), queries[i][0], queries[i][1], 1);
+                else
+                    results[i] = -1.0;
+
+            return results;
+        }
+
+        private double DFS(Dictionary<string, List<Tuple<string, double>>> dict, HashSet<string> visited, string cur, string e, double val)
+        {
+            if (cur == e)
+                return val;
+
+            if (dict.ContainsKey(cur))
+                foreach (var item in dict[cur])
+                    if (!visited.Contains(item.Item1))
+                    {
+                        visited.Add(item.Item1);
+
+                        double res = DFS(dict, visited, item.Item1, e, val * item.Item2);
+
+                        if (res != -1.0)
+                            return res;
+                    }
+
+            return -1.0;
+        }
         #endregion
         #endregion
 
