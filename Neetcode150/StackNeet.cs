7a53f14fa82e59ff1c4f43a9c76cc3ed740875ab
cdincer <dincercan@gmail.com>
2023-12-25 12:57:37 +0300

    Largest Rectangle in Histogram WIP

--- Neetcode150/StackNeet.cs
+++ Neetcode150/StackNeet.cs
@@ -550,6 +550,26 @@ namespace Neetcode150
         /* WIP
         https://leetcode.com/problems/largest-rectangle-in-histogram/
         */
+        public int LargestRectangleArea(int[] heights)
+        {
+            int n = heights.Length, maxa = 0;
+            var stack = new Stack<int>();
+
+            for (int i = 0; i <= heights.Length; i++)
+            {
+                int h = (i == heights.Length) ? 0 : heights[i];
+
+                while (stack.Count > 0 && h < heights[stack.Peek()])
+                {
+                    int height = heights[stack.Pop()];
+                    int width = (stack.Count == 0) ? i : i - stack.Peek() - 1;
+                    maxa = Math.Max(maxa, height * width);
+                }
+                stack.Push(i);
+            }
+
+            return maxa;
+        }
         #endregion
 
     }
