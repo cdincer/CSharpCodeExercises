29933593762089acff581ecb5c79a5cc435830ac
cdincer <dincercan@gmail.com>
2024-01-02 08:39:24 +0300

    Find Minimum in Rotated Sorted Array solution change

--- Neetcode150/BinarySearchNeet.cs
+++ Neetcode150/BinarySearchNeet.cs
@@ -33,7 +33,10 @@ namespace Neetcode150
         public int Search(int[] nums, int target)
         {
             int left = 0, right = nums.Length - 1;
-
+            //left:0///right 5
+            //mid first run 2.5 == 2
+            //left = 2
+            //second run  3+2 
             while (left <= right)
             {
                 int mid = left + ((right - left) / 2); // (left + right) / 2 can lead to overflow
@@ -180,39 +183,30 @@ namespace Neetcode150
         [2,1] 108 / 150 testcases
         https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
         */
-        //Custom solution and neetcode's solution are quite similar in runtime,memory usage numbers.
-        //After adding the additional conditional for comparing 0th index and Length-1 Index, when this added
-        //Runtime significantly gets better than neetcodes.
         public int FindMin(int[] nums)
         {
-            int left = 0;
-            int right = nums.Length - 1;
-            int resultMin = int.MaxValue;
+            int s = 0;
+            int e = nums.Length - 1;
+            int minValue = nums[0];
 
-            if (nums.Length == 1 && nums[left] <= nums[right])
-                return nums[0];
+            if (nums[s] < nums[e])
+                return nums[s];
 
-            while (left <= right)
+            while (s <= e)
             {
-                int mid = left + ((right - left) / 2);
-                resultMin = Math.Min(resultMin, nums[right]);
-
-                if (nums[mid] > nums[left])
+                int mid = s + ((e - s) / 2);
+                if (nums[mid] < minValue)
                 {
-                    resultMin = Math.Min(resultMin, nums[left]);
-                    left = mid + 1;
-                }
-                else if (nums[mid] < nums[right])
-                {
-                    resultMin = Math.Min(resultMin, nums[mid]);
-                    right = mid - 1;
+                    e = mid - 1;
+                    minValue = nums[mid];
                 }
                 else
                 {
-                    left = mid + 1;
+                    s = mid + 1;
                 }
             }
-            return resultMin;
+
+            return minValue;
         }
         public int FindMinNeet(int[] nums)
         {
@@ -256,8 +250,6 @@ namespace Neetcode150
         Input: nums = [1], target = 0
         Output: -1
 
-        
-
         Constraints:
 
             1 <= nums.length <= 5000
@@ -266,40 +258,45 @@ namespace Neetcode150
             nums is an ascending array that is possibly rotated.
             -104 <= target <= 104
 
-
         Extra Test Cases:
-        [1]
-        2
-        [1]
-        -1
-        [1,2]
-        1
-        [2,1]
-        1
+
         [3,5,1] 125 / 195
         3
         [4,5,6,7,8,1,2,3] 165 / 195
         8
+        [3,5,1] 170 / 195
+        1
         [3,4,5,6,7,1,2] 174 / 195
         4
+        [8,1,2,3,4,5,6,7] 182 / 195
+        6
         */
         public int Search2(int[] nums, int target)
         {
+            int low = 0;
+            int high = nums.Length - 1;
 
-            if (nums.Length == 1 && nums[0] == target)
-                return 0;
-            else if (nums.Length == 1 && nums[0] != target)
-                return -1;
-
-            int left = 0;
-            int right = nums.Length - 1;
-
-            while (left <= right)
+            while (low <= high)
             {
-                int mid = left + ((right - left) / 2);
+                var mid = (low + high) / 2;
+
                 if (nums[mid] == target)
                     return mid;
 
+                if (nums[low] <= nums[mid])
+                {
+                    if (target > nums[mid] || target < nums[low])
+                        low = mid + 1;
+                    else
+                        high = mid - 1;
+                }
+                else
+                {
+                    if (target < nums[mid] || target > nums[high])
+                        high = mid - 1;
+                    else
+                        low = mid + 1;
+                }
             }
             return -1;
         }
--- Tier1/BinarySearch.cs
+++ Tier1/BinarySearch.cs
@@ -12,6 +12,27 @@ namespace Tier1
         You must write an algorithm with O(log n) runtime complexity.
         https://leetcode.com/problems/binary-search/
         */
+
+        /*
+        Template #1 is the most basic and elementary form of Binary Search. 
+        It is the standard Binary Search Template that most high schools or universities use when they first teach students computer science.
+        Template #1 is used to search for an element or condition which can be determined by accessing a single index in the array.
+
+        Key Attributes:
+
+            Most basic and elementary form of Binary Search
+            Search Condition can be determined without comparing to the element's neighbors (or use specific elements around it)
+            No post-processing required because at each step, you are checking to see if the element has been found. If you reach the end, then you know the element is not found
+
+        Distinguishing Syntax:
+
+            Initial Condition: left = 0, right = length-1
+            Termination: left > right
+            Searching Left: right = mid-1
+            Searching Right: left = mid+1
+
+        */
+        //Below is Template 1 starter
         public int BinarySearchOriginal(int[] nums, int target)
         {
             int pivot, left = 0, right = nums.Length;
@@ -21,8 +42,8 @@ namespace Tier1
                 pivot = left + (right - left) / 2;
 
                 if (nums[pivot] == target) return pivot;
-                if (nums[pivot] > target) right = pivot - 1;
-                else { left = pivot + 1; }
+                if (nums[pivot] > target) right = pivot - 1; //search left lower end.
+                else { left = pivot + 1; } //search right higher end.
             }
 
             return 0;
