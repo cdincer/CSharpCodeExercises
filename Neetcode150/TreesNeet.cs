84fcea8f7b92849ffe2214a26fa5458cae94328b
cdincer <dincercan@gmail.com>
2024-01-19 15:32:58 +0300

    Binary Tree Maximum Path Sum beginning

--- Neetcode150/TreesNeet.cs
+++ Neetcode150/TreesNeet.cs
@@ -547,7 +547,6 @@ namespace Neetcode150
         [2,2,2]
         [0]
         [1,null,1]
-        [5,4,6,null,null,3,7]
         [0,null,1]
         [3,1,5,0,2,4,6,null,null,null,3]
         [3,1,5,0,2,4,6]
@@ -573,7 +572,24 @@ namespace Neetcode150
         #endregion
         #region Kth Smallest Element in a BST
         /*
-        
+        Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+        Example 1:
+        Input: root = [3,1,4,null,2], k = 1
+        Output: 1
+
+        Example 2:
+        Input: root = [5,3,6,2,4,null,null,1], k = 3
+        Output: 3
+
+        Constraints:
+
+            The number of nodes in the tree is n.
+            1 <= k <= n <= 104
+            0 <= Node.val <= 104
+
+        Follow up: If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth smallest frequently, how would you optimize?
+        https://leetcode.com/problems/kth-smallest-element-in-a-bst/
         */
         //Custom Solution, runtime and memory consumption is really close to Neetcode's.
         Stack<int> items = new();
@@ -628,6 +644,25 @@ namespace Neetcode150
         #endregion
         #region Construct Binary Tree from Preorder and Inorder Traversal
         /*
+        Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+        Example 1:
+        Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+        Output: [3,9,20,null,null,15,7]
+
+        Example 2:
+        Input: preorder = [-1], inorder = [-1]
+        Output: [-1]
+
+        Constraints:
+        1 <= preorder.length <= 3000
+        inorder.length == preorder.length
+        -3000 <= preorder[i], inorder[i] <= 3000
+        preorder and inorder consist of unique values.
+        Each value of inorder also appears in preorder.
+        preorder is guaranteed to be the preorder traversal of the tree.
+        inorder is guaranteed to be the inorder traversal of the tree.
+
         https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
         */
         public TreeNode BuildTree(int[] preorder, int[] inorder)
@@ -650,6 +685,53 @@ namespace Neetcode150
             rootNode.right = BuildTreeHelper(preStart + mid - inStart + 1, mid + 1, inEnd, preorder, inorder);
 
             return rootNode;
+        }
+        #endregion
+        #region Binary Tree Maximum Path Sum
+        /*
+        A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once.
+        Note that the path does not need to pass through the root.
+        The path sum of a path is the sum of the node's values in the path.
+        Given the root of a binary tree, return the maximum path sum of any non-empty path.
+
+        Example 1:
+        Input: root = [1,2,3]
+        Output: 6
+        Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+
+        Example 2:
+        Input: root = [-10,9,20,null,null,15,7]
+        Output: 42
+        Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
+
+        Constraints:
+
+            The number of nodes in the tree is in the range [1, 3 * 104].
+            -1000 <= Node.val <= 1000
+
+        https://leetcode.com/problems/binary-tree-maximum-path-sum/
+        */
+        int maxPathSum = Int32.MinValue;
+
+        public int MaxPathSum(TreeNode root)
+        {
+            DfsMaxPathSum(root);
+            return maxPathSum;
+        }
+
+        private int DfsMaxPathSum(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            int leftMax = DfsMaxPathSum(root.left),
+                rightMax = DfsMaxPathSum(root.right),
+                currentMax = 0;
+
+            currentMax = Math.Max(currentMax, Math.Max(leftMax + root.val, rightMax + root.val));
+            maxPathSum = Math.Max(maxPathSum, leftMax + root.val + rightMax);
+
+            return currentMax;
         }
         #endregion
     }
