4caa835729b47cb3094d322785b0d18c32140e14
cdincer <dincercan@gmail.com>
2024-01-25 11:42:00 +0300

    Word Search beginning

--- Neetcode150/BacktrackingNeet.cs
+++ Neetcode150/BacktrackingNeet.cs
@@ -330,5 +330,144 @@ namespace Neetcode150
             }
         }
         #endregion
+        #region Word Search
+        /*
+        Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+        The word can be constructed from letters of sequentially adjacent cells, 
+        where adjacent cells are horizontally or vertically neighboring. 
+        The same letter cell may not be used more than once.
+
+        Example 1:
+        Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+        Output: true
+
+        Example 2:
+        Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+        Output: true
+
+        Example 3:
+        Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+        Output: false
+
+        Constraints:
+
+            m == board.length
+            n = board[i].length
+            1 <= m, n <= 6
+            1 <= word.length <= 15
+            board and word consists of only lowercase and uppercase English letters.
+
+        Follow up: Could you use search pruning to make your solution faster with a larger board?
+
+        https://leetcode.com/problems/word-search/
+        */
+        //Adapted solution from TriesNeet Word Search II.
+        //run time: 246 ms beats 87.00% memory:42.7mb beats 27.5%
+        public class Node
+        {
+            public Node[] next = new Node['z' + 1];
+            public string Word;
+        }
+        public bool Exist(char[][] board, string word)
+        {
+            Node root = new Node();
+
+            Node temp = root;
+            for (int i = 0; i < word.Length; i++)
+            {
+                char ch = word[i];
+                if (temp.next[ch] == null)
+                {
+                    temp.next[ch] = new Node();
+                }
+                temp = temp.next[ch];
+            }
+            temp.Word = word;
+
+            int rl = board.Length;
+            int cl = board[0].Length;
+            bool result = false;
+            for (int i = 0; i < rl; i++)
+            {
+                Node seek = root;
+                for (int j = 0; j < cl; j++)
+                {
+                    if (result == true)
+                        break;
+
+                    searcher(i, j, seek);
+                }
+            }
+
+            void searcher(int r, int c, Node curr)
+            {
+                if (r < 0 || c < 0 || r == rl || c == cl)
+                    return;
+
+                char tch = board[r][c];
+                if (curr.next[tch] == null || board[r][c] == '/')
+                    return;
+
+                curr = curr.next[tch];
+                if (curr.Word != null && curr.Word == word)
+                {
+                    result = true;
+                    return;
+                }
+                board[r][c] = '/';
+                searcher(r - 1, c, curr);
+                searcher(r + 1, c, curr);
+                searcher(r, c - 1, curr);
+                searcher(r, c + 1, curr);
+                board[r][c] = tch;
+
+            }
+            return result;
+        }
+        #endregion
+        #region Palindrome Partitioning
+        /*
+        
+        */
+        public IList<IList<string>> Partition(string s)
+        {
+            List<IList<string>> results = new();
+            List<string> result = new();
+            dfs(0);
+            void dfs(int i)
+            {
+                if (i >= s.Length)
+                {
+                    results.Add(result.ToList());
+                    return;
+                }
+
+                for (int j = i; j < s.Length; j++)
+                {
+                    if (palindrome(s, i, j))
+                    {
+                        result.Add(s.Substring(i, j - i + 1));
+                        dfs(j + 1);
+                        result.RemoveAt(result.Count - 1);
+                    }
+                }
+            }
+
+            bool palindrome(string word, int begin, int end)
+            {
+                while (begin < end)
+                {
+                    if (word[begin] != word[end])
+                        return false;
+
+                    begin++;
+                    end--;
+                }
+                return true;
+            }
+
+            return results;
+        }
+        #endregion
     }
 }
