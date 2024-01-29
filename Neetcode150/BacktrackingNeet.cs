402fb1d80d502fe7622e9f15969dfe0f73174d03
cdincer <dincercan@gmail.com>
2024-01-29 13:56:20 +0300

    Letter Combinations of a Phone Number completed

--- Neetcode150/BacktrackingNeet.cs
+++ Neetcode150/BacktrackingNeet.cs
@@ -490,8 +490,31 @@ namespace Neetcode150
         #endregion
         #region Letter Combinations of a Phone Number
         /*
+        Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. 
+        Return the answer in any order.
+        A mapping of digits to letters (just like on the telephone buttons) is given below. 
+        Note that 1 does not map to any letters.
+
+        
+        Example 1:
+        Input: digits = "23"
+        Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+        Example 2:
+        Input: digits = ""
+        Output: []
+
+        Example 3:
+        Input: digits = "2"
+        Output: ["a","b","c"]
+
+        Constraints:
+            0 <= digits.length <= 4
+            digits[i] is a digit in the range ['2', '9'].
+
         https://leetcode.com/problems/letter-combinations-of-a-phone-number/
         */
+        //Keeping this solution in because it fits the other backtracking questions solution style.
         public IList<string> LetterCombinations(string digits)
         {
             var lettersMap = new Dictionary<char, string>
