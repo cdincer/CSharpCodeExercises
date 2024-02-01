86fc6d78696b022589d744c37167c742db2c17e2
cdincer <dincercan@gmail.com>
2024-02-01 18:52:18 +0300

    Last Stone Weight completed

--- Neetcode150/HeapPriorityQueueNeet.cs
+++ Neetcode150/HeapPriorityQueueNeet.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 
 namespace Neetcode150
 {
@@ -66,6 +67,33 @@ namespace Neetcode150
         #endregion
         #region Last Stone Weight
         /*
+        You are given an array of integers stones where stones[i] is the weight of the ith stone.
+        We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+
+            If x == y, both stones are destroyed, and
+            If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+
+        At the end of the game, there is at most one stone left.
+        Return the weight of the last remaining stone. If there are no stones left, return 0.
+
+        Example 1:
+        Input: stones = [2,7,4,1,8,1]
+        Output: 1
+        Explanation: 
+        We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+        we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+        we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+        we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
+
+        Example 2:
+        Input: stones = [1]
+        Output: 1
+
+        Constraints:
+
+        1 <= stones.length <= 30
+        1 <= stones[i] <= 1000
+
         https://leetcode.com/problems/last-stone-weight/
         */
 
@@ -95,6 +123,36 @@ namespace Neetcode150
         }
 
         #endregion
+        #region K Closest Points to Origin
+        /*
+        
+        Extra Test Cases: 
+        [[-95,76],[17,7],[-55,-58],[53,20],[-69,-8],[-57,87],[-2,-42],[-10,-87],[-36,-57],[97,-39],[97,49]] 63 / 87 testcases passed
+        */
+        public int[][] KClosest(int[][] points, int k)
+        {
+            var items = points.Select(point =>
+            {
+                long x = point[0];
+                long y = point[1];
+
+                return (point, x * x + y * y);
+            });
+
+            int[][] result = new int[k][];
+            // T: O(n)
+            PriorityQueue<int[], long> queue = new(items);
+
+            // T: O(k log(n))
+            for (int i = 0; i < k; i++)
+            {
+                result[i] = queue.Dequeue();
+            }
+
+            return result;
+        }
+        #endregion
+
     }
 
 }
