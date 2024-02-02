8e8d37c3cad7d965f54d59231c460b8237ddbaf2
cdincer <dincercan@gmail.com>
2024-02-02 22:05:20 +0300

    Design Twitter test cases added, Task Scheduler completed

--- Neetcode150/HeapPriorityQueueNeet.cs
+++ Neetcode150/HeapPriorityQueueNeet.cs
@@ -200,6 +200,45 @@ namespace Neetcode150
 
         #endregion
         #region Task Scheduler
+        /*
+        Given a characters array tasks, representing the tasks a CPU needs to do, where each letter represents a different task. Tasks could be done in any order. Each task is done in one unit of time. 
+        For each unit of time, the CPU could complete either one task or just be idle.
+        However, there is a non-negative integer n that represents the cooldown period between two same tasks (the same letter in the array), 
+        that is that there must be at least n units of time between any two same tasks.
+        Return the least number of units of times that the CPU will take to finish all the given tasks.
+
+        Example 1:
+        Input: tasks = ["A","A","A","B","B","B"], n = 2
+        Output: 8
+        Explanation: 
+        A -> B -> idle -> A -> B -> idle -> A -> B
+        There is at least 2 units of time between any two same tasks.
+
+        Example 2:
+        Input: tasks = ["A","A","A","B","B","B"], n = 0
+        Output: 6
+        Explanation: On this case any permutation of size 6 would work since n = 0.
+        ["A","A","A","B","B","B"]
+        ["A","B","A","B","A","B"]
+        ["B","B","B","A","A","A"]
+        ...
+        And so on.
+
+        Example 3:
+        Input: tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"], n = 2
+        Output: 16
+        Explanation: 
+        One possible solution is
+        A -> B -> C -> A -> D -> E -> A -> F -> G -> A -> idle -> idle -> A -> idle -> idle -> A
+
+        Constraints:
+
+            1 <= task.length <= 104
+            tasks[i] is upper-case English letter.
+            The integer n is in the range [0, 100].
+
+        https://leetcode.com/problems/task-scheduler/
+        */
         public int LeastInterval(char[] tasks, int n)
         {
             Dictionary<char, int> freq = new();
@@ -246,6 +285,14 @@ namespace Neetcode150
             return time;
         }
         #endregion
-    }
+        #region Design Twitter
+        /*
+        ["Twitter","postTweet","getNewsFeed","follow","getNewsFeed","unfollow","getNewsFeed"]  1 / 16 testcases passed
+        [[],[1,1],[1],[2,1],[2],[2,1],[2]]
+        ["Twitter","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","postTweet","getNewsFeed","follow","getNewsFeed","unfollow","getNewsFeed"] 14 / 16 testcases passed
+        [[],[1,5],[2,3],[1,101],[2,13],[2,10],[1,2],[1,94],[2,505],[1,333],[2,22],[1,11],[1,205],[2,203],[1,201],[2,213],[1,200],[2,202],[1,204],[2,208],[2,233],[1,222],[2,211],[1],[1,2],[1],[1,2],[1]]
+        */
 
+        #endregion
+    }
 }
