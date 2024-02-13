b598fdafc241d75524a77118a284b249cf06b8f6
cdincer <dincercan@gmail.com>
2024-02-13 18:31:34 +0300

    Course Schedule completed

--- Neetcode150/GraphNeet.cs
+++ Neetcode150/GraphNeet.cs
@@ -569,12 +569,143 @@ namespace Neetcode150
         #endregion
         #region Course Schedule
         /*
+        There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] 
+        indicates that you must take course bi first if you want to take course ai.
+        For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+
+        Return true if you can finish all courses. Otherwise, return false.
+
+        Example 1:
+        Input: numCourses = 2, prerequisites = [[1,0]]
+        Output: true
+        Explanation: There are a total of 2 courses to take. 
+        To take course 1 you should have finished course 0. So it is possible.
+
+        Example 2:
+        Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+        Output: false
+        Explanation: There are a total of 2 courses to take. 
+        To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+
+        Constraints:
+
+            1 <= numCourses <= 2000
+            0 <= prerequisites.length <= 5000
+            prerequisites[i].length == 2
+            0 <= ai, bi < numCourses
+            All the pairs prerequisites[i] are unique.
 
         https://leetcode.com/problems/course-schedule/
         Test Cases:
         numCourses = 5
         prerequisites = [[1,4],[2,4],[3,1],[3,2]]
         */
+        public bool CanFinish(int numCourses, int[][] prerequisites)
+        {
+            Dictionary<int, List<int>> mdict = new();
+
+            for (int i = 0; i < numCourses; i++)
+            {
+                mdict[i] = new List<int>();
+            }
+
+            for (int i = 0; i < prerequisites.Length; i++)
+            {
+                int ToTake = prerequisites[i][0];
+                int ToDepend = prerequisites[i][1];
+
+                mdict[ToTake].Add(ToDepend);
+            }
+            HashSet<int> visited = new();
+            foreach (var item in mdict)
+            {
+                if (!dfs(mdict, visited, item.Key))
+                    return false;
+            }
+            return true;
+
+            bool dfs(Dictionary<int, List<int>> preq, HashSet<int> vis, int curs)
+            {
+                if (vis.Contains(curs))
+                    return false;
+
+                if (preq[curs] == new List<int>())
+                    return true;
+
+                vis.Add(curs);
+                foreach (var elo in preq[curs])
+                {
+                    if (!dfs(preq, vis, elo))
+                        return false;
+                }
+                vis.Remove(curs);
+                preq[curs] = new List<int>();
+
+                return true;
+            }
+        }
+        #endregion
+        #region Course Schedule II
+        /*
+        Test Cases:
+        numCourses = 2 43 / 45 testcases passed
+        prerequisites = []
+        https://leetcode.com/problems/course-schedule-ii/
+        */
+        public int[] FindOrder(int numCourses, int[][] prerequisites)
+        {
+
+            Dictionary<int, List<int>> mdict = new();
+            HashSet<int> visited = new();
+            HashSet<int> cycle = new();
+            List<int> results = new();
+
+            for (int i = 0; i < numCourses; i++)
+            {
+                mdict[i] = new List<int>();
+            }
+
+            for (int i = 0; i < prerequisites.Length; i++)
+            {
+                int ToTake = prerequisites[i][0];
+                int ToDepend = prerequisites[i][1];
+
+                mdict[ToTake].Add(ToDepend);
+            }
+
+
+            foreach (var item in mdict)
+            {
+                if (!dfs(mdict, visited, cycle, item.Key))
+                    return new int[0];
+            }
+            bool dfs(Dictionary<int, List<int>> preq, HashSet<int> vis, HashSet<int> cycle, int curs)
+            {
+                if (vis.Contains(curs))
+                    return false;
+
+                if (preq[curs] == new List<int>())
+                    return true;
+
+                if (cycle.Contains(curs))
+                    return true;
+
+                vis.Add(curs);
+                cycle.Add(curs);
+
+                foreach (var elo in preq[curs])
+                {
+                    if (!dfs(preq, vis, cycle, elo))
+                        return false;
+                }
+                vis.Remove(curs);
+                results.Add(curs);
+                preq[curs] = new List<int>();
+                return true;
+            }
+
+            return results.ToArray();
+        }
         #endregion
     }
 }
