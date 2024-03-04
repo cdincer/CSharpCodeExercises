f052f65ce5bebaacc07be86f212a53437578dd32
cdincer <dincercan@gmail.com>
2024-03-04 20:06:39 +0300

    House Robber 2 completed

--- Neetcode150/DpOneNeet.cs
+++ Neetcode150/DpOneNeet.cs
@@ -57,19 +57,81 @@ namespace Neetcode150
 
             return Math.Max(dp[nums.Length - 1], dp[nums.Length - 2]);
         }
-            public int RobNeet(int[] nums) {
-            int rob1 =0;
-            int rob2 =0;
+        public int RobNeet(int[] nums)
+        {
+            int rob1 = 0;
+            int rob2 = 0;
 
-            for(int i=0;i<nums.Length;i++)
+            for (int i = 0; i < nums.Length; i++)
             {
-                int temp = Math.Max(rob1+nums[i],rob2);
+                int temp = Math.Max(rob1 + nums[i], rob2);
                 rob1 = rob2;
                 rob2 = temp;
             }
             return rob2;
         }
         #endregion
+        #region House Robber 2
+        /*
+        You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically 
+        contact the police if two adjacent houses were broken into on the same night.
+        Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+        Example 1:
+        Input: nums = [2,3,2]
+        Output: 3
+        Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+
+        Example 2:
+        Input: nums = [1,2,3,1]
+        Output: 4
+        Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+        Total amount you can rob = 1 + 3 = 4.
+
+        Example 3:
+        Input: nums = [1,2,3]
+        Output: 3
+
+        Constraints:
+
+            1 <= nums.length <= 100
+            0 <= nums[i] <= 1000
+
+        Test Cases:
+        nums= [2,7,9,3,1] 48 / 75 test cases passed
+        nums = [1] 73/75 test cases passed
+
+        https://leetcode.com/problems/house-robber-ii/
+        */
+        //52 ms on March 04th 2024
+        public int Rob2(int[] nums)
+        {
+
+            if (nums.Length == 1)
+                return nums[0];
+
+            if (nums.Length == 2)
+                return Math.Max(nums[0], nums[1]);
+
+            return Math.Max(cycles(nums, 0, nums.Length - 1), cycles(nums, 1, nums.Length));
+        }
+        public int cycles(int[] nums, int begin, int end)
+        {
+            int rob1 = 0;
+            int rob2 = 0;
+
+            for (int i = begin; i < end; i++)
+            {
+                int temp = Math.Max(rob1 + nums[i], rob2);
+                rob1 = rob2;
+                rob2 = temp;
+            }
+
+            return rob2;
+        }
+
+        #endregion
+
     }
 
 }
