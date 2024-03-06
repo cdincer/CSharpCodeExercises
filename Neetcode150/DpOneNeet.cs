20679a44161bdc1cfbbc182e4df897cdaa195f04
cdincer <dincercan@gmail.com>
2024-03-06 19:14:40 +0300

    Palindromic Substrings completed

--- Neetcode150/DpOneNeet.cs
+++ Neetcode150/DpOneNeet.cs
@@ -180,6 +180,53 @@ namespace Neetcode150
             return right - left - 1;
         }
         #endregion
-    }
+        #region Palindromic Substrings
+        /*
+        Given a string s, return the number of palindromic substrings in it.
+        A string is a palindrome when it reads the same backward as forward.
+        A substring is a contiguous sequence of characters within the string.
 
+        Example 1:
+        Input: s = "abc"
+        Output: 3
+        Explanation: Three palindromic strings: "a", "b", "c".
+
+        Example 2:
+        Input: s = "aaa"
+        Output: 6
+        Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+
+        Constraints:
+            1 <= s.length <= 1000
+            s consists of lowercase English letters.
+
+            https://leetcode.com/problems/palindromic-substrings/
+        */
+        public int CountSubstrings(string s)
+        {
+            var count = 0;
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                count += getPalindromeCount(s, i, i);
+                count += getPalindromeCount(s, i, i + 1);
+            }
+
+            return count;
+        }
+
+        public int getPalindromeCount(string s, int l, int r)
+        {
+            var count = 0;
+
+            while (l >= 0 && r < s.Length && s[l] == s[r])
+            {
+                count++;
+                l--;
+                r++;
+            }
+            return count;
+        }
+        #endregion
+    }
 }
