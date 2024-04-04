7acefcaf491c612dbfd466d613f895e38ebe65d0
cdincer <dincercan@gmail.com>
2024-04-04 18:24:10 +0300

    Unique Paths completed, mixed places corrected

--- Neetcode150/DpOneNeet.cs
+++ Neetcode150/DpOneNeet.cs
@@ -485,7 +485,23 @@ namespace Neetcode150
         #endregion
         #region Partition Equal Subset Sum
         /*
-        
+        Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
+
+        Example 1:
+        Input: nums = [1,5,11,5]
+        Output: true
+        Explanation: The array can be partitioned as [1, 5, 5] and [11].
+
+        Example 2:
+        Input: nums = [1,2,3,5]
+        Output: false
+        Explanation: The array cannot be partitioned into equal sum subsets.
+
+        Constraints:
+
+            1 <= nums.length <= 200
+            1 <= nums[i] <= 100
+
         [23,13,11,7,6,5,5]
         [1,1]
         [6,14,19,10,17,10,8,15,16,1,12,4,9,2,15]
@@ -495,6 +511,53 @@ namespace Neetcode150
         [1,5,10,6] 140 / 142 testcases passed
         https://leetcode.com/problems/partition-equal-subset-sum/
         */
+        public bool CanPartition(int[] nums)
+        {
+
+
+            var sum = nums.Sum();
+            if (sum % 2 != 0)
+            {
+                return false;
+            }
+
+            return subSetSum(nums, sum / 2);
+        }
+
+        private bool subSetSum(int[] nums, int target)
+        {
+            var dp = new bool[nums.Length + 1, target + 1];
+
+            for (var i = 0; i < nums.Length + 1; i++)
+            {
+                for (var j = 0; j < target + 1; j++)
+                {
+                    if (i == 0)
+                    {
+                        dp[i, j] = false;
+                    }
+                    if (j == 0)
+                    {
+                        dp[i, j] = true;
+                    }
+                }
+            }
+
+            for (var i = 1; i < nums.Length + 1; i++)
+            {
+                for (var j = 1; j < target + 1; j++)
+                {
+                    if (nums[i - 1] <= j)
+                    {
+                        dp[i, j] = dp[i - 1, j] || dp[i - 1, j - nums[i - 1]];
+                    }
+                    else
+                        dp[i, j] = dp[i - 1, j];
+                }
+            }
+
+            return dp[nums.Length, target];
+        }
         #endregion
     }
 }
--- Neetcode150/DpTwoNeet.cs
+++ Neetcode150/DpTwoNeet.cs
@@ -6,24 +6,48 @@ namespace Neetcode150
 {
     public class DpTwoNeet
     {
-        #region Longest Common Subsequence
+        #region Unique Paths
         /*
-        Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
-
+        There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). 
+        The robot can only move either down or right at any point in time. Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+        The test cases are generated so that the answer will be less than or equal to 2 * 109.
+       
         Example 1:
-        Input: nums = [1,5,11,5]
-        Output: true
-        Explanation: The array can be partitioned as [1, 5, 5] and [11].
+        Input: m = 3, n = 7
+        Output: 28
 
         Example 2:
-        Input: nums = [1,2,3,5]
-        Output: false
-        Explanation: The array cannot be partitioned into equal sum subsets.
+        Input: m = 3, n = 2
+        Output: 3
+        Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+        1. Right -> Down -> Down
+        2. Down -> Down -> Right
+        3. Down -> Right -> Down
 
         Constraints:
+            1 <= m, n <= 100
 
-            1 <= nums.length <= 200
-            1 <= nums[i] <= 100
+        */
+        public int UniquePaths(int m, int n)
+        {
+            int[] row = new int[n];
+            Array.Fill(row, 1);
+
+            for (int i = 0; i < m - 1; i++)
+            {
+                var newrow = new int[n];
+                Array.Fill(newrow, 1);
+                for (int r = n - 2; r >= 0; r--)
+                {
+                    newrow[r] = newrow[r + 1] + row[r];
+                }
+                row = newrow;
+            }
+            return row[0];
+        }
+        #endregion
+        #region Longest Common Subsequence
+        /*
 
         Extra Test Cases:
         "a"
@@ -44,53 +68,28 @@ namespace Neetcode150
         "qzqqqqqzzqqqqzqqzqzqqqqqqqzqqqzqqzzqqqqzzzzqqqqzqqqqzzqqzqqqqqzqzqzqzqzqqqzqzqqqqqqzzqqqqqqzqzzqqqqzqqqzqzzqqzqzqqzqz
         https://leetcode.com/problems/longest-common-subsequence/
         */
-        public bool CanPartition(int[] nums)
+        public int LongestCommonSubsequence(string text1, string text2)
         {
+            string longW = text1.Length > text2.Length ? text1 : text2;
+            string shortW = text1.Length > text2.Length ? text2 : text1;
 
+            int[,] dp = new int[longW.Length + 1, shortW.Length + 1];
 
-            var sum = nums.Sum();
-            if (sum % 2 != 0)
+            for (int r = longW.Length - 1; r >= 0; r--)
             {
-                return false;
-            }
-
-            return subSetSum(nums, sum / 2);
-        }
-
-        private bool subSetSum(int[] nums, int target)
-        {
-            var dp = new bool[nums.Length + 1, target + 1];
-
-            for (var i = 0; i < nums.Length + 1; i++)
-            {
-                for (var j = 0; j < target + 1; j++)
+                for (int c = shortW.Length - 1; c >= 0; c--)
                 {
-                    if (i == 0)
-                    {
-                        dp[i, j] = false;
-                    }
-                    if (j == 0)
-                    {
-                        dp[i, j] = true;
-                    }
-                }
-            }
-
-            for (var i = 1; i < nums.Length + 1; i++)
-            {
-                for (var j = 1; j < target + 1; j++)
-                {
-                    if (nums[i - 1] <= j)
-                    {
-                        dp[i, j] = dp[i - 1, j] || dp[i - 1, j - nums[i - 1]];
-                    }
+                    if (longW[r] == shortW[c])
+                        dp[r, c] = 1 + dp[r + 1, c + 1];
                     else
-                        dp[i, j] = dp[i - 1, j];
+                        dp[r, c] = Math.Max(dp[r, c + 1], dp[r + 1, c]);
+
                 }
             }
 
-            return dp[nums.Length, target];
+            return dp[0, 0];
         }
+       
         #endregion
     }
     
