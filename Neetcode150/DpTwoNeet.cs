5140a02d16badf924fff40317a156152ea4a8ad5
cdincer <dincercan@gmail.com>
2024-04-18 19:40:30 +0300

    Longest Increasing Path in a Matrix completed

--- Neetcode150/DpTwoNeet.cs
+++ Neetcode150/DpTwoNeet.cs
@@ -265,9 +265,37 @@ namespace Neetcode150
         #endregion
         #region Longest Increasing Path in a Matrix
         /*
+        Given an m x n integers matrix, return the length of the longest increasing path in matrix.
+        From each cell, you can either move in four directions: left, right, up, or down. You may not move diagonally or move outside the boundary (i.e., wrap-around is not allowed).
+
+        Example 1:
+        Input: matrix = [[9,9,4],[6,6,8],[2,1,1]]
+        Output: 4
+        Explanation: The longest increasing path is [1, 2, 6, 9].
+
+        Example 2:
+        Input: matrix = [[3,4,5],[3,2,6],[2,2,1]]
+        Output: 4
+        Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
+
+        Example 3:
+        Input: matrix = [[1]]
+        Output: 1
+
+        Constraints:
+
+            m == matrix.length
+            n == matrix[i].length
+            1 <= m, n <= 200
+            0 <= matrix[i][j] <= 231 - 1
+
+        Code below can use a 2d array and it became 40% faster than dictionary.
+        
         https://leetcode.com/problems/longest-increasing-path-in-a-matrix/description/
         Extra Test Case:
         [[7,0,8],[4,7,8],[4,7,4]] 17 / 139 testcases passed
+        135 / 139 testcases passed
+        [[0,1,2,3,4,5,6,7,8,9],[19,18,17,16,15,14,13,12,11,10],[20,21,22,23,24,25,26,27,28,29],[39,38,37,36,35,34,33,32,31,30],[40,41,42,43,44,45,46,47,48,49],[59,58,57,56,55,54,53,52,51,50],[60,61,62,63,64,65,66,67,68,69],[79,78,77,76,75,74,73,72,71,70],[80,81,82,83,84,85,86,87,88,89],[99,98,97,96,95,94,93,92,91,90],[100,101,102,103,104,105,106,107,108,109],[119,118,117,116,115,114,113,112,111,110],[120,121,122,123,124,125,126,127,128,129],[139,138,137,136,135,134,133,132,131,130],[0,0,0,0,0,0,0,0,0,0]]
         */
         public int LongestIncreasingPath(int[][] matrix)
         {
