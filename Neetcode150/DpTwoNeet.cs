629fb05ba4ad8f853246c6f52af88ad1d13c9d59
cdincer <dincercan@gmail.com>
2024-04-30 18:47:19 +0300

    Burst Balloons complete

--- Neetcode150/DpTwoNeet.cs
+++ Neetcode150/DpTwoNeet.cs
@@ -550,6 +550,47 @@ namespace Neetcode150
             0 <= nums[i] <= 100
         https://leetcode.com/problems/burst-balloons/
         */
+
+        public int MaxCoins(int[] nums)
+        {
+            List<int> numsl = nums.ToList();
+            numsl.Insert(0, 1);
+            numsl.Add(1);
+            int n = numsl.Count;
+            int[] newArray = numsl.ToArray();
+            int[][] mem = new int[n][];
+
+            for (int i = 0; i < mem.Length; i++)
+            {
+                mem[i] = new int[n];
+                for (int j = 0; j < mem[i].Length; j++)
+                {
+                    mem[i][j] = -1;
+                }
+            }
+            return MCM(newArray, 1, n - 1, mem);
+        }
+
+        public int MCM(int[] nums, int left, int right, int[][] mem)
+        {
+            if (left >= right)
+                return 0;
+
+            if (mem[left][right] != -1)
+                return mem[left][right];
+
+            int max_cost = int.MinValue;
+            for (int k = left; k < right; k++)
+            {
+                int total_cost = MCM(nums, left, k, mem) + MCM(nums, k + 1, right, mem) + nums[left - 1] * nums[k] * nums[right];  //after traversing any element from left to right, 
+                                                                                                                                   //Assume that the traversed element is no more in nums array and find product of current element and its adjacent element
+                max_cost = Math.Max(max_cost, total_cost);
+
+                mem[left][right] = max_cost;
+            }
+            return mem[left][right];
+        }
+
         #endregion
     }
 
