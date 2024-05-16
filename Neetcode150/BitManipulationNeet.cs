cfa5e863ba318a7f1dd97c843a39f371bf567b9e
cdincer <dincercan@gmail.com>
2024-05-16 19:47:52 +0300

    Reverse Integer completed

--- Neetcode150/BitManipulationNeet.cs
+++ Neetcode150/BitManipulationNeet.cs
@@ -216,38 +216,51 @@ namespace Neetcode150
         #endregion
         #region Reverse Integer
         /*
+        Given a signed 32-bit integer x, return x with its digits reversed. 
+        If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+        Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+        Example 1:
+        Input: x = 123
+        Output: 321
+
+        Example 2:
+        Input: x = -123
+        Output: -321
+
+        Example 3:
+        Input: x = 120
+        Output: 21
+
+        Constraints:
+
+            -231 <= x <= 231 - 1
+
+        https://leetcode.com/problems/reverse-integer
+
         Extra Test Cases:
         1534236469 1036 / 1045 testcases passed
         */
-          public int Reverse(int x) {
-        int result = 0;
+            public int Reverse(int x)
+            {
+                int result = 0;
+                while (x != 0)
+                {
+                    int digit = x % 10;
 
-        if(x > int.MaxValue || x < int.MinValue)
-        return 0;
+                    if (result > int.MaxValue / 10 || (result == int.MaxValue && digit > 8))
+                        return 0;
 
-        while( x != 0)
-        {
-            if(x * 10 > int.MaxValue || x * 10 < int.MinValue)
-            return 0;
+                    if (result < int.MinValue / 10 || (result == int.MinValue && digit < -7))
+                        return 0;
 
-            if(x / 10 > int.MaxValue || x / 10 < int.MinValue)
-            return 0;
-            int remain = x % 10;
-            x = x / 10;
-            
-            result = (result * 10) + remain;
-        }
-//2147483648
-//1534236469
-//9646324351
+                    x = x / 10;
+                    result = (result * 10) + digit;
+                }
 
-            if(result > int.MaxValue ||result < int.MinValue)
-            return 0;
-
-           
-        Console.WriteLine("hit");
-        return result;
-    }
+                return result;
+            }
+        
         #endregion
     }
 }
