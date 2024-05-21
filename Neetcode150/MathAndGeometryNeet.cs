1c1dbee43f4acc0e62970a8a9c4c0c931876f702
cdincer <dincercan@gmail.com>
2024-05-21 18:33:09 +0300

    Spiral Matrix completed

--- Neetcode150/MathAndGeometryNeet.cs
+++ Neetcode150/MathAndGeometryNeet.cs
@@ -55,9 +55,72 @@ namespace Neetcode150
         #endregion
         #region Spiral Matrix
         /*
+        Given an m x n matrix, return all elements of the matrix in spiral order.
+
+        Example 1:
+        Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+        Output: [1,2,3,6,9,8,7,4,5]
+
+        Example 2:
+        Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+        Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+        Constraints:
+
+            m == matrix.length
+            n == matrix[i].length
+            1 <= m, n <= 10
+            -100 <= matrix[i][j] <= 100
+
+
         Extra Test Cases: 
         [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]] 15 / 25 testcases passed
+        https://leetcode.com/problems/spiral-matrix/
         */
+        public IList<int> SpiralOrder(int[][] matrix)
+        {
+            List<int> result = new List<int>();
+            int top = 0;
+            int left = 0;
+            int right = matrix[0].Length - 1;
+            int bottom = matrix.Length - 1;
+
+            while (true)
+            {
+                //Left to Right
+                for (int i = left; i <= right; i++)
+                {
+                    result.Add(matrix[top][i]);
+                }
+                top++;
+                if (left > right || top > bottom) break;
+
+                //Top to Bottom
+                for (int i = top; i <= bottom; i++)
+                {
+                    result.Add(matrix[i][right]);
+                }
+                right--;
+                if (left > right || top > bottom) break;
+
+                //Right to Left
+                for (int i = right; i >= left; i--)
+                {
+                    result.Add(matrix[bottom][i]);
+                }
+                bottom--;
+                if (left > right || top > bottom) break;
+
+                //Bottom to Top
+                for (int i = bottom; i >= top; i--)
+                {
+                    result.Add(matrix[i][left]);
+                }
+                left++;
+                if (left > right || top > bottom) break;
+            }//Repeat
+            return result;
+        }
         #endregion
     }
 }
