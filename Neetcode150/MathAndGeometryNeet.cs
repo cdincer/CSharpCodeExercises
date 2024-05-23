2089e9b5243c882faf6fd3562bc016f3fcb806b2
cdincer <dincercan@gmail.com>
2024-05-23 17:50:54 +0300

    Set Matrix Zeroes completed

--- Neetcode150/MathAndGeometryNeet.cs
+++ Neetcode150/MathAndGeometryNeet.cs
@@ -124,10 +124,57 @@ namespace Neetcode150
         #endregion
         #region Set Matrix Zeroes
         /*
+        Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+        You must do it in place.
+
+        Example 1:
+        Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+        Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+        Example 2:
+        Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+        Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+        Constraints:
+
+            m == matrix.length
+            n == matrix[0].length
+            1 <= m, n <= 200
+            -231 <= matrix[i][j] <= 231 - 1
+
+        Follow up:
+
+            A straightforward solution using O(mn) space is probably a bad idea.
+            A simple improvement uses O(m + n) space, but still not the best solution.
+            Could you devise a constant space solution?
+
         https://leetcode.com/problems/set-matrix-zeroes/
         Extra Test Cases:
+        [[1],[0]] 136 / 193 testcases passed
         [[-4,-2147483648,6,-7,0],[-8,6,-8,-6,0],[2147483647,2,-9,-6,-10]] 163 / 193 testcases passed
-        */        
+        */
+        public void SetZeroes(int[][] matrix)
+        {
+            int rl = matrix.Length, cl = matrix[0].Length;
+            bool firstRowHasZeros = matrix[0].Contains(0);
+
+            for (int r = 1; r < rl; r++)
+                for (int c = 0; c < cl; c++)
+                    if (matrix[r][c] == 0)
+                        matrix[r][0] = matrix[0][c] = 0;
+
+            for (int r = 1; r < rl; r++)
+                if (matrix[r][0] == 0)
+                    Array.Fill(matrix[r], 0);
+
+            for (int c = 0; c < cl; c++)
+                if (matrix[0][c] == 0)
+                    for (int r = 0; r < rl; r++)
+                        matrix[r][c] = 0;
+
+            if (firstRowHasZeros)
+                Array.Fill(matrix[0], 0);
+        }
         #endregion Set Matrix Zeroes
 
     }
