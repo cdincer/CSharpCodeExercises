daadf5426b0ce80a8f7831475e1455b4ca9b671e
cdincer <dincercan@gmail.com>
2024-05-27 19:25:56 +0300

    Plus One extra test cases added

--- Neetcode150/MathAndGeometryNeet.cs
+++ Neetcode150/MathAndGeometryNeet.cs
@@ -205,7 +205,7 @@ namespace Neetcode150
         Constraints:
 
             1 <= n <= 231 - 1
-            
+
         https://leetcode.com/problems/happy-number/
         n = 4   4 / 420 testcases passed
         n = 1   9 / 420 testcases passed
@@ -244,5 +244,56 @@ namespace Neetcode150
             }
         }
         #endregion Happy Number
+        #region Plus One
+        /*
+        You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. 
+        The large integer does not contain any leading 0's.
+        Increment the large integer by one and return the resulting array of digits.
+
+        Example 1:
+        Input: digits = [1,2,3]
+        Output: [1,2,4]
+        Explanation: The array represents the integer 123.
+        Incrementing by one gives 123 + 1 = 124.
+        Thus, the result should be [1,2,4].
+
+        Example 2:
+        Input: digits = [4,3,2,1]
+        Output: [4,3,2,2]
+        Explanation: The array represents the integer 4321.
+        Incrementing by one gives 4321 + 1 = 4322.
+        Thus, the result should be [4,3,2,2].
+
+        Example 3:
+
+        Input: digits = [9]
+        Output: [1,0]
+        Explanation: The array represents the integer 9.
+        Incrementing by one gives 9 + 1 = 10.
+        Thus, the result should be [1,0].
+
+        Constraints:
+
+            1 <= digits.length <= 100
+            0 <= digits[i] <= 9
+            digits does not contain any leading 0's.
+
+
+        digits = [1,2,3] 4 / 111 testcases passed
+        digits = [1,0,0,0,0] 50 / 111 testcases passed
+        digits = [9] 100 / 111 testcases passed
+        digits = [8,9,9,9] 108 / 111 testcases passed Sep 16th 2022
+        digits = [1] 101 / 109 testcases passed
+        digits = [9,9] 108 / 111 testcases passed May 27th 2024
+        */
+
+        #endregion Plus One
+        #region Pow(x, n)
+        /*
+        x = 0.44528 n = 0 285 / 307 testcases passed
+        x = 1.00000 n = -2147483648 300 / 307 testcases passed
+        */
+        #endregion Pow(x, n)
+
     }
 }
