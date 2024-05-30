6ff5c8aeb468f09b3d862e263f07fc3f8972fe71
cdincer <dincercan@gmail.com>
2024-05-30 19:12:54 +0300

    Pow(x, n) completed

--- Neetcode150/MathAndGeometryNeet.cs
+++ Neetcode150/MathAndGeometryNeet.cs
@@ -313,11 +313,52 @@ namespace Neetcode150
         #endregion Plus One
         #region Pow(x, n)
         /*
+        Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+        Example 1:
+        Input: x = 2.00000, n = 10
+        Output: 1024.00000
+
+        Example 2:
+        Input: x = 2.10000, n = 3
+        Output: 9.26100
+
+        Example 3:
+        Input: x = 2.00000, n = -2
+        Output: 0.25000
+        Explanation: 2-2 = 1/22 = 1/4 = 0.25
+
+        Constraints:
+            -100.0 < x < 100.0
+            -231 <= n <= 231-1
+            n is an integer.
+            Either x is not zero or n > 0.
+            -104 <= xn <= 104
+
         x = 0.44528 n =  0 285 / 307 testcases passed
         x = 1.00000 n = -2147483648 291 / 306 testcases passed
         x = 0.00001 n = 2147483647 300 / 307 testcases passed
         x= -1.00000 n = 2147483647
+        https://leetcode.com/problems/powx-n/
         */
+        public double MyPow(double x, long n)
+        {
+            x = helper(x, Math.Abs(n));
+            return n > 0 ? x : 1 / x;
+        }
+
+        public double helper(double x, long n)
+        {
+            if (n == 0)
+                return 1;
+
+            if (x == 0)
+                return 0;
+
+            double result = helper(x, n / 2);
+            result = result * result;
+            return n % 2 == 1 ? result * x : result;
+        }
         #endregion Pow(x, n)
 
     }
