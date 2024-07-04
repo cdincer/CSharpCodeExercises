bf1ff3f044a8c41e45b7586edf256dd3faf36617
cdincer <dincercan@gmail.com>
2024-07-04 19:09:00 +0300

    Meeting Rooms completed

--- Neetcode150/IntervalsNeet.cs
+++ Neetcode150/IntervalsNeet.cs
@@ -214,6 +214,63 @@ namespace Neetcode150
             return del;
         }
         #endregion
+        #region Meeting Rooms
+        /*
+        Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] (start_i < end_i), determine if a person could add all meetings to their schedule without any conflicts.
+        
+        Example 1:
+        Input: intervals = [(0,30),(5,10),(15,20)]
+        Output: false
+        Explanation:
+
+            (0,30) and (5,10) will conflict
+            (0,30) and (15,20) will conflict
+
+        Example 2:
+        Input: intervals = [(5,8),(9,15)]
+        Output: true
+        Note:
+            (0,8),(8,10) is not considered a conflict at 8
+        Constraints:
+
+            0 <= intervals.length <= 500
+            0 <= intervals[i].start < intervals[i].end <= 1,000,000
+
+        */
+        public class Interval
+        {
+            public int start, end;
+            public Interval(int start, int end)
+            {
+                this.start = start;
+                this.end = end;
+            }
+        }
+ 
+        public bool CanAttendMeetings(List<Interval> intervals)
+        {
+            Interval[] arri = intervals.ToArray();
+
+            if (arri.Length == 1 || arri.Length == 0)
+                return true;
+
+            Array.Sort(arri, (a, b) => a.start - b.start);
+            int prevEnd = arri[0].end;
+
+            for (int i = 1; i < arri.Length; i++)
+            {
+                if (prevEnd > arri[i].start)
+                    return false;
+                else
+                    prevEnd = arri[i].end;
+            }
+
+
+            return true;
+        }
+        #endregion
+      
+      
         #region Minimum Interval to Include Each Query
         /*
         https://leetcode.com/problems/minimum-interval-to-include-each-query/
