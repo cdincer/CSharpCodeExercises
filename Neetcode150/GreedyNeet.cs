568b6767d44355c762f9eb91b098dfb271380bd8
cdincer <dincercan@gmail.com>
2024-07-23 18:19:00 +0300

    Gas Station completed

--- Neetcode150/GreedyNeet.cs
+++ Neetcode150/GreedyNeet.cs
@@ -129,11 +129,66 @@ namespace Neetcode150
         #endregion
         #region Gas Station
         /*
+        There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+        You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. 
+        You begin the journey with an empty tank at one of the gas stations. Given two integer arrays gas and cost, 
+        return the starting gas station's index if you can travel around the circuit once in the clockwise direction,
+         otherwise return -1. If there exists a solution, it is guaranteed to be unique
+
+        Example 1:
+        Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+        Output: 3
+        Explanation:
+        Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+        Travel to station 4. Your tank = 4 - 1 + 5 = 8
+        Travel to station 0. Your tank = 8 - 2 + 1 = 7
+        Travel to station 1. Your tank = 7 - 3 + 2 = 6
+        Travel to station 2. Your tank = 6 - 4 + 3 = 5
+        Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+        Therefore, return 3 as the starting index.
+
+        Example 2:
+        Input: gas = [2,3,4], cost = [3,4,3]
+        Output: -1
+        Explanation:
+        You can't start at station 0 or 1, as there is not enough gas to travel to the next station.
+        Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+        Travel to station 0. Your tank = 4 - 3 + 2 = 3
+        Travel to station 1. Your tank = 3 - 3 + 3 = 3
+        You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
+        Therefore, you can't travel around the circuit once no matter where you start.
+
+        Constraints:
+            n == gas.length == cost.length
+            1 <= n <= 105
+            0 <= gas[i], cost[i] <= 104
+
         Extra Test Cases:
         gas=[2,0,0]
         cost=[0,1,0]
         https://leetcode.com/problems/gas-station/
         */
+        public int CanCompleteCircuit(int[] gas, int[] cost)
+        {
+            int total = 0;
+            int loc = 0;
+
+            if (cost.Sum() > gas.Sum())
+                return -1;
+
+            for (int i = 0; i < gas.Length; i++)
+            {
+                total += gas[i] - cost[i];
+
+                if (0 > total)
+                {
+                    loc = i + 1;
+                    total = 0;
+                }
+            }
+
+            return loc;
+        }
         #endregion
         #region Hand Of Straights
         /*
@@ -144,6 +199,7 @@ namespace Neetcode150
         [8,10,12] 3 = false
         [66,75,4,37,92,87,68,65,58,100,97,42,19,66,73,1,5,44,30,29,76,31,12,35,26,93,9,36,90,16,86,15,4,9,13,98,10,14,18,90,89,3,10,65,24,31,43,25,54,55,54,81,10,80,31,12,15,14,59,27,64,93,32,26,69,79,13,32,29,24,27,91,92,82,37,101,100,61,74,30,91,62,36,92,28,23,4,63,55,3,11,11,101,22,34,25,2,75,43,72] 2 
         */
+
         #endregion
     }
 
