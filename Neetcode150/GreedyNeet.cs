e34fb373771188d8ac60bcd60720be7817101c92
cdincer <dincercan@gmail.com>
2024-07-24 18:56:45 +0300

    Hand Of Straights completed

--- Neetcode150/GreedyNeet.cs
+++ Neetcode150/GreedyNeet.cs
@@ -192,6 +192,26 @@ namespace Neetcode150
         #endregion
         #region Hand Of Straights
         /*
+        Alice has some number of cards and she wants to rearrange the cards into groups so that each group is of size groupSize, and consists of groupSize consecutive cards.
+        Given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize, return true if she can rearrange the cards, or false otherwise.
+
+        Example 1:
+        Input: hand = [1,2,3,6,2,3,4,7,8], groupSize = 3
+        Output: true
+        Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8]
+
+        Example 2:
+        Input: hand = [1,2,3,4,5], groupSize = 4
+        Output: false
+        Explanation: Alice's hand can not be rearranged into groups of 4.
+
+        Constraints:
+            1 <= hand.length <= 104
+            0 <= hand[i] <= 109
+            1 <= groupSize <= hand.length
+
+        Note: This question is the same as 1296: https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/
+
         Hand Of Straights 
         [2,3,4,5,6,7,8,9] 8 
         [8,8,9,7,7,7,6,7,10,6] 2 
@@ -199,7 +219,46 @@ namespace Neetcode150
         [8,10,12] 3 = false
         [66,75,4,37,92,87,68,65,58,100,97,42,19,66,73,1,5,44,30,29,76,31,12,35,26,93,9,36,90,16,86,15,4,9,13,98,10,14,18,90,89,3,10,65,24,31,43,25,54,55,54,81,10,80,31,12,15,14,59,27,64,93,32,26,69,79,13,32,29,24,27,91,92,82,37,101,100,61,74,30,91,62,36,92,28,23,4,63,55,3,11,11,101,22,34,25,2,75,43,72] 2 
         */
+        public bool IsNStraightHand(int[] hand, int groupSize)
+        {
+            if (hand.Length % groupSize != 0)
+                return false;
 
+
+            var dictionary = new Dictionary<int, int>();
+            var minHeap = new PriorityQueue<int, int>();
+            foreach (var item in hand)
+            {
+                dictionary.TryAdd(item, 0);
+                dictionary[item]++;
+            }
+
+            // heapify is linear algorithm
+            foreach (var key in dictionary.Keys)
+                minHeap.Enqueue(key, key);
+
+            while (minHeap.Count > 0)
+            {
+                var first = minHeap.Peek();
+
+                for (var i = first; i < first + groupSize; i++)
+                {
+                    if (!dictionary.ContainsKey(i))
+                        return false;
+
+                    dictionary[i]--;
+                    if (dictionary[i] == 0)
+                        if (i != minHeap.Peek())
+                            return false;
+                        else
+                            minHeap.Dequeue();
+                }
+
+            }
+
+            return true;
+
+        }
         #endregion
     }
 
