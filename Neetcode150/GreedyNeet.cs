b5722ec32e864c7d4ebb85495a6de563bed6ec73
cdincer <dincercan@gmail.com>
2024-07-26 19:22:43 +0300

    Partition Labels completed, Valid Parenthesis String extra test cases added

--- Neetcode150/GreedyNeet.cs
+++ Neetcode150/GreedyNeet.cs
@@ -320,7 +320,77 @@ namespace Neetcode150
             return hashSet.Count == 3;
         }
         #endregion
+        #region Partition Labels
+        /*
+        You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part.
+        Note that the partition is done so that after concatenating all the parts in order, the resultant string should be s.
+        Return a list of integers representing the size of these parts.
 
+        Example 1:
+        Input: s = "ababcbacadefegdehijhklij"
+        Output: [9,7,8]
+        Explanation:
+        The partition is "ababcbaca", "defegde", "hijhklij".
+        This is a partition so that each letter appears in at most one part.
+        A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+
+        Example 2:
+        Input: s = "eccbbbbdec"
+        Output: [10]
+
+        Constraints:
+            1 <= s.length <= 500
+            s consists of lowercase English letters.
+        
+        Extra Test Cases:
+        "qiejxqfnqceocmy" [13,1,1] 
+        "abaccbdeffed" [6,6]
+        https://leetcode.com/problems/partition-labels/description/
+        */
+        public IList<int> PartitionLabels(string s)
+        {
+
+            var chars = new int[26];
+            for (var ch = 0; ch < s.Length; ch++)
+            {
+                chars[s[ch] - 'a'] = ch;
+            }
+
+            var result = new List<int>();
+            var end = 0;
+            var size = 0;
+            for (var i = 0; i < s.Length; i++)
+            {
+                end = Math.Max(chars[s[i] - 'a'], end);
+                size++;
+                if (i == end)
+                {
+                    result.Add(size);
+                    size = 0;
+                }
+
+            }
+
+            return result;
+        }
+
+        #endregion
+        #region Valid Parenthesis String
+        /*
+        "((**(((((" false 
+        "()()" true
+        "((" false 
+        "(((((((((((((" false 
+        "((((()(()()()()((((()()((())))))(())()())(((())())())))))))(((((()))))()))(()((()()))()()" true 62 / 83 testcases passed 
+        "((((((()((((((*(((()())()()()(((()())))))))))(())(()))())((()()(((()((()(())(()**)()(())" false 61 / 83 testcases passed
+        "" "((()*()((()" 
+        "(()())()()*((()())()((()*()" 
+        ")(()(*()())))())()))()()((()()))(*())*(" 
+        ")))())))))))))(((()(((())(*)(((()))()((((()(((" 
+        "()))))*)(()()*)))())()())()())*())))(*())))())))()(((" 
+        "()())(())))()())())()(()))*)())(()()((())()((())))()*)((()))((()((())()"
+        */
+        #endregion
     }
 
 }
