cdfe8d288e6cb01d74d93c463489874256c1652a
cdincer <dincercan@gmail.com>
2024-08-05 18:31:52 +0300

    Network Delay Time begin, extra test cases added

--- Neetcode150/AdvancedGraphsNeet.cs
+++ Neetcode150/AdvancedGraphsNeet.cs
@@ -83,15 +83,10 @@ namespace Neetcode150
         #region Min Cost to Connect All Points
         /*
         You are given an array points representing integer coordinates of some points on a 2D-plane, where points[i] = [xi, yi].
-
         The cost of connecting two points [xi, yi] and [xj, yj] is the manhattan distance between them: |xi - xj| + |yi - yj|, where |val| denotes the absolute value of val.
-
         Return the minimum cost to make all points connected. All points are connected if there is exactly one simple path between any two points.
 
-        
-
         Example 1:
-
         Input: points = [[0,0],[2,2],[3,10],[5,2],[7,0]]
         Output: 20
         Explanation: 
@@ -100,7 +95,6 @@ namespace Neetcode150
         Notice that there is a unique path between every pair of points.
 
         Example 2:
-
         Input: points = [[3,12],[-2,5],[-4,1]]
         Output: 18
 
@@ -157,6 +151,12 @@ namespace Neetcode150
         }
 
         #endregion
-
+        #region Network Delay Time
+        /*
+        Extra Test Cases:
+        [[1,2,1],[2,3,2]] 8/53 testcases
+        [[1,2,1],[2,3,7],[1,3,4],[2,1,2]] 50/53 testcases
+        */
+        #endregion
     }
 }
