89359da3cb31032bdc0308bfbf480b4cd0c44379
cdincer <dincercan@gmail.com>
2024-08-13 18:11:34 +0300

    Network Delay Time completed,Alien Dictionary extra test case

--- Neetcode150/AdvancedGraphsNeet.cs
+++ Neetcode150/AdvancedGraphsNeet.cs
@@ -156,6 +156,32 @@ namespace Neetcode150
         #endregion
         #region Network Delay Time
         /*
+        You are given a network of n nodes, labeled from 1 to n. You are also given times, a list of travel times as directed edges times[i] = (ui, vi, wi), where ui is the source node, vi is the target node, 
+        and wi is the time it takes for a signal to travel from source to target.
+        We will send a signal from a given node k. Return the minimum time it takes for all the n nodes to receive the signal. If it is impossible for all the n nodes to receive the signal, return -1.
+
+        Example 1:
+        Input: times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2
+        Output: 2
+
+        Example 2:
+        Input: times = [[1,2,1]], n = 2, k = 1
+        Output: 1
+
+        Example 3:
+        Input: times = [[1,2,1]], n = 2, k = 2
+        Output: -1
+
+        Constraints:
+
+            1 <= k <= n <= 100
+            1 <= times.length <= 6000
+            times[i].length == 3
+            1 <= ui, vi <= n
+            ui != vi
+            0 <= wi <= 100
+            All the pairs (ui, vi) are unique. (i.e., no multiple edges.)
+
         Extra Test Cases:
         [[1,2,1],[2,3,2]] n = 3 k = 1 Output: 3 8/53 testcases
         [[1,2,1],[2,3,2],[1,3,4]] n = 3 k = 1 Output: 3 13 / 53 testcases
@@ -210,6 +236,12 @@ namespace Neetcode150
         /*
         Extra Test Case:
         [[3,2],[0,1]] Output: 3 30 / 43 testcases passed
+        */
+        #endregion
+        #region Alien Dictionary
+        /*
+        Extra Test Case:
+        words=["wrt","wrf","er","ett","rftt","te"] Expected Output: wertf
         */
         #endregion
     }
