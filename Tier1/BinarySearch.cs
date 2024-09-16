0154ef17dbf432496d4f97cdf63960cd27aef209
cdincer <dincercan@gmail.com>
2024-09-16 19:38:09 +0300

    Search in Rotated Sorted Array test cases formatted properly, fixed the name for the same question in Tier1

--- Tier1/BinarySearch.cs
+++ Tier1/BinarySearch.cs
@@ -109,7 +109,7 @@ namespace Tier1
             return 0;
         }
         #endregion
-        #region Search rotated Array      
+        #region Search in Rotated Sorted Array      
         /*
         There is an integer array nums sorted in ascending order (with distinct values).
         Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is:
