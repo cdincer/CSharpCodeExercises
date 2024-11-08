f49f4d72e93d1366ca46a582a323a92f10a75f7d
cdincer <dincercan@gmail.com>
2024-11-08 20:09:30 +0300

    Maximum Width Ramp

--- Exercises/Leetcode/Stack Related/StackLeetCode1.cs
+++ Exercises/Leetcode/Stack Related/StackLeetCode1.cs
@@ -108,5 +108,52 @@ namespace Exercises.Leetcode.StackRelated
             return actualResult;
         }
         #endregion
+        #region Maximum Width Ramp
+        /*
+        A ramp in an integer array nums is a pair (i, j) for which i < j and nums[i] <= nums[j]. 
+        The width of such a ramp is j - i. Given an integer array nums, return the maximum width of a ramp in nums. 
+        If there is no ramp in nums, return 0.
+
+        Example 1:
+        Input: nums = [6,0,8,2,1,5]
+        Output: 4
+        Explanation: The maximum width ramp is achieved at (i, j) = (1, 5): nums[1] = 0 and nums[5] = 5.
+
+        Example 2:
+        Input: nums = [9,8,1,0,1,9,4,0,4,1]
+        Output: 7
+        Explanation: The maximum width ramp is achieved at (i, j) = (2, 9): nums[2] = 1 and nums[9] = 1.
+
+        Constraints:
+
+            2 <= nums.length <= 5 * 104
+            0 <= nums[i] <= 5 * 104
+
+        Extra Test Cases:
+        nums = [2,2,1] Expected: 1 73 / 101 testcases passed
+        nums = [1,0] Expected: 0 68 / 101 testcases passed
+        nums = [4,2,3,1] Expected: 1 65 / 101 testcases passed
+        https://leetcode.com/problems/maximum-width-ramp/
+        */
+        public int MaxWidthRamp(int[] nums)
+        {
+            Stack<int> mstack = new();
+            int res = 0, n = nums.Length;
+            for (int i = 0; i < n; ++i)
+            {
+                if (mstack.Count == 0 || nums[mstack.Peek()] > nums[i])
+                    mstack.Push(i);
+            }
+              
+            for (int i = n - 1; i > res; --i)
+            {
+                while (mstack.Count != 0 && nums[mstack.Peek()] <= nums[i])
+                    res = Math.Max(res, i - mstack.Pop());
+            }
+
+            return res;
+        }
+        #endregion
+
     }
 }
