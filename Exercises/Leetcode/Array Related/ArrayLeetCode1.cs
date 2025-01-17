159a57bd45f7cabfff89555784dbdcb9f37791b8
cdincer <dincercan@gmail.com>
2025-01-17 04:57:12 +0300

    Word Subsets added

--- Exercises/Leetcode/Array Related/ArrayLeetCode1.cs
+++ Exercises/Leetcode/Array Related/ArrayLeetCode1.cs
@@ -43,6 +43,90 @@ namespace Exercises.Leetcode.ArrayRelated
             return Math.Min(decreaseR, increaseR);
         }
         #endregion
+        #region Word Subsets
+        /*
+        You are given two string arrays words1 and words2.
+        A string b is a subset of string a if every letter in b occurs in a including multiplicity.
+
+            For example, "wrr" is a subset of "warrior" but is not a subset of "world".
+
+        A string a from words1 is universal if for every string b in words2, b is a subset of a.
+        Return an array of all the universal strings in words1. You may return the answer in any order.
+
+        Example 1:
+        Input: words1 = ["amazon","apple","facebook","google","leetcode"], words2 = ["e","o"]
+        Output: ["facebook","google","leetcode"]
+
+        Example 2:
+        Input: words1 = ["amazon","apple","facebook","google","leetcode"], words2 = ["lc","eo"]
+        Output: ["leetcode"]
+
+        Example 3:
+        Input: words1 = ["acaac","cccbb","aacbb","caacc","bcbbb"], words2 = ["c","cc","b"]
+        Output: ["cccbb"]
+
+        Constraints:
+
+            1 <= words1.length, words2.length <= 104
+            1 <= words1[i].length, words2[i].length <= 10
+            words1[i] and words2[i] consist only of lowercase English letters.
+            All the strings of words1 are unique.
+
+        https://leetcode.com/problems/word-subsets/
+        */
+        public IList<string> WordSubsets(string[] words1, string[] words2)
+        {
+            List<int[]> comparisonInfo = new();
+
+            foreach (string word in words2)
+            {
+                int[] tempComparison = new int[26];
+                foreach (char c in word)
+                {
+                    int cLetter = c - 'a';
+                    tempComparison[cLetter]++;
+                }
+                comparisonInfo.Add(tempComparison);
+            }
+
+            int[] allFinished = new int[26];
+            foreach (int[] set in comparisonInfo)
+            {
+
+                for (int i = 0; i < set.Length; i++)
+                    allFinished[i] = Math.Max(allFinished[i], set[i]);
+            }
+
+            List<string> result = new();
+            foreach (string word in words1)
+            {
+                bool fullCompliance = true;
+                int[] compare = new int[26];
+                for (int i = 0; i < word.Length; i++)
+                {
+                    int cLetter = word[i] - 'a';
+                    compare[cLetter]++;
+                }
+
+                for (int i = 0; i < allFinished.Length; i++)
+                {
+                    if (compare[i] >= allFinished[i])
+                        continue;
+
+                    fullCompliance = false;
+                    break;
+                }
+
+                if (fullCompliance)
+                {
+                    result.Add(word);
+                }
+            }
+
+            return result;
+        }
+        #endregion
+
 
 
     }
