e2605731133b2ad4ee43c15f588553013061fb6b
cdincer <dincercan@gmail.com>
2025-01-31 20:34:22 +0300

    Longest Increasing Path in a Matrix replaced previous solution

--- Neetcode150/DpTwoNeet.cs
+++ Neetcode150/DpTwoNeet.cs
@@ -407,46 +407,63 @@ namespace Neetcode150
         135 / 139 testcases passed
         [[0,1,2,3,4,5,6,7,8,9],[19,18,17,16,15,14,13,12,11,10],[20,21,22,23,24,25,26,27,28,29],[39,38,37,36,35,34,33,32,31,30],[40,41,42,43,44,45,46,47,48,49],[59,58,57,56,55,54,53,52,51,50],[60,61,62,63,64,65,66,67,68,69],[79,78,77,76,75,74,73,72,71,70],[80,81,82,83,84,85,86,87,88,89],[99,98,97,96,95,94,93,92,91,90],[100,101,102,103,104,105,106,107,108,109],[119,118,117,116,115,114,113,112,111,110],[120,121,122,123,124,125,126,127,128,129],[139,138,137,136,135,134,133,132,131,130],[0,0,0,0,0,0,0,0,0,0]]
         */
+        private int[][] memo;
+        //TO-DO
+        //Needs refactoring and benchmarking between jagged array and 2D array.
+        //Variable:memo
         public int LongestIncreasingPath(int[][] matrix)
         {
+            int m = matrix.Length;
+            int n = matrix[0].Length;
 
-            var rows = matrix.Length;
-            var cols = matrix[0].Length;
-            var maxValue = int.MinValue;
-
-            var dictionary = new Dictionary<(int, int), int>();
-
-            int dfs(int r, int c, int previous)
+            memo = new int[m][];
+            for (int i = 0; i < m; i++)
             {
-                if (r >= matrix.Length || c >= matrix[0].Length || r < 0 || c < 0 || matrix[r][c] <= previous)
-                    return 0;
-
-                if (dictionary.ContainsKey((r, c)))
-                    return dictionary[(r, c)];
-
-                var value = new int[]
-                            {
-                             dfs(r + 1, c, matrix[r][c]),
-                             dfs(r - 1, c, matrix[r][c]),
-                             dfs(r, c - 1, matrix[r][c]),
-                             dfs(r, c + 1, matrix[r][c])
-                            }.Max() + 1;
-
-                dictionary.TryAdd((r, c), 0);
-                dictionary[(r, c)] = value;
-
-                maxValue = Math.Max(maxValue, value);
-                return value;
+                memo[i] = new int[n];
             }
 
-            for (var r = 0; r < rows; r++)
+            int max = 0;
+            for (int i = 0; i < m; i++)
             {
-                for (var c = 0; c < cols; c++)
+                for (int j = 0; j < n; j++)
                 {
-                    dfs(r, c, -1);
+                    max = Math.Max(max, DFS(matrix, i, j));
                 }
             }
-            return maxValue;
+            return max;
+        }
+        private int DFS(int[][] matrix, int i, int j)
+        {
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+            if (memo[i][j] != 0)
+            {
+                return memo[i][j];
+            }
+
+            int max = 1;
+            // up
+            if (i - 1 >= 0 && matrix[i - 1][j] > matrix[i][j])
+            {
+                max = Math.Max(max, 1 + DFS(matrix, i - 1, j));
+            }
+            // down
+            if (i + 1 < m && matrix[i + 1][j] > matrix[i][j])
+            {
+                max = Math.Max(max, 1 + DFS(matrix, i + 1, j));
+            }
+            // left
+            if (j - 1 >= 0 && matrix[i][j - 1] > matrix[i][j])
+            {
+                max = Math.Max(max, 1 + DFS(matrix, i, j - 1));
+            }
+            // right
+            if (j + 1 < n && matrix[i][j + 1] > matrix[i][j])
+            {
+                max = Math.Max(max, 1 + DFS(matrix, i, j + 1));
+            }
+            memo[i][j] = max;
+            return max;
         }
         #endregion
         #region Distinct Subsequences
