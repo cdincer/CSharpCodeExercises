82559ace820982404e0348da73dc9c687f93202d
cdincer <dincercan@gmail.com>
2025-02-21 08:45:43 +0300

    Longest Increasing Path in a Matrix added converted C# test cases

--- Neetcode150/DpTwoNeet.cs
+++ Neetcode150/DpTwoNeet.cs
@@ -373,7 +373,7 @@ namespace Neetcode150
             return dp[0];
         }
         #endregion
-        #region Longest Increasing Path in a Matrix
+        #region  Increasing Path in a Matrix
         /*
         Given an m x n integers matrix, return the length of the longest increasing path in matrix.
         From each cell, you can either move in four directions: left, right, up, or down. You may not move diagonally or move outside the boundary (i.e., wrap-around is not allowed).
@@ -406,6 +406,23 @@ namespace Neetcode150
         [[7,0,8],[4,7,8],[4,7,4]] 17 / 139 testcases passed
         135 / 139 testcases passed
         [[0,1,2,3,4,5,6,7,8,9],[19,18,17,16,15,14,13,12,11,10],[20,21,22,23,24,25,26,27,28,29],[39,38,37,36,35,34,33,32,31,30],[40,41,42,43,44,45,46,47,48,49],[59,58,57,56,55,54,53,52,51,50],[60,61,62,63,64,65,66,67,68,69],[79,78,77,76,75,74,73,72,71,70],[80,81,82,83,84,85,86,87,88,89],[99,98,97,96,95,94,93,92,91,90],[100,101,102,103,104,105,106,107,108,109],[119,118,117,116,115,114,113,112,111,110],[120,121,122,123,124,125,126,127,128,129],[139,138,137,136,135,134,133,132,131,130],[0,0,0,0,0,0,0,0,0,0]]
+        
+        C# Converted Sample Test Case:
+        [[9,9,4],[6,6,8],[2,1,1]]
+         int[][] twoDArray = new int[][]
+            {
+            new int[] {9,9,4},
+            new int[] {6,6,8},
+            new int[] {2,1,1}
+            };
+
+        [[3,4,5],[3,2,6],[2,2,1]]
+         int[][] twoDArray = new int[][]
+            {
+            new int[] {3,4,5},
+            new int[] {3,2,6},
+            new int[] {2,2,1}
+            };
         */
         private int[][] memo;
         //TO-DO
@@ -620,7 +637,6 @@ namespace Neetcode150
             0 <= nums[i] <= 100
         https://leetcode.com/problems/burst-balloons/
         */
-        
         public int MaxCoins(int[] nums)
         {
             int n = nums.Length;
@@ -643,8 +659,6 @@ namespace Neetcode150
 
             return dp[1, n];
         }
-
-
         #endregion
         #region Regular Expression Matching
         /*
