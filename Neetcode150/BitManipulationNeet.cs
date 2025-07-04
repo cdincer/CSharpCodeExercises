a76a57d07157ec9a856316115a16cb8951186ff1
cdincer <dincercan@gmail.com>
2025-07-04 20:33:42 +0300

    Sum Of Two Integers appropriate solution added

--- Neetcode150/BitManipulationNeet.cs
+++ Neetcode150/BitManipulationNeet.cs
@@ -249,23 +249,13 @@ namespace Neetcode150
         */
         public int GetSum(int a, int b)
         {
-            int res = 0;
-            int carry = 0;
-
-            for (int i = 0; i < 32; i++)
+            while (b != 0)
             {
-                int a_bit = (a >> i) & 1;
-                int b_bit = (b >> i) & 1;
-
-                int curr_bit = a_bit ^ b_bit ^ carry;
-                carry = (a_bit + b_bit + carry) >= 2 ? 1 : 0;
-
-                if (curr_bit != 0)
-                    res |= (1 << i);
-
+                int carry = (a & b) << 1;
+                a ^= b;
+                b = carry;
             }
-
-            return res;
+            return a;
         }
         #endregion
         #region Reverse Integer
