d2666113b0e08146e3d4056881fc276bac70932d
cdincer <dincercan@gmail.com>
2025-10-09 06:08:24 +0300

    Find Minimum in Rotated Sorted Array solution replaced with Lower Bound Binary Search solution

--- Neetcode150/BinarySearchNeet.cs
+++ Neetcode150/BinarySearchNeet.cs
@@ -198,36 +198,75 @@ namespace Neetcode150
         #endregion
         #region Find Minimum in Rotated Sorted Array
         /*
+        You are given an array of length n which was originally sorted in ascending order. It has now been rotated between 1 and n times. For example, the array nums = [1,2,3,4,5,6] might become:
+
+            [3,4,5,6,1,2] if it was rotated 4 times.
+            [1,2,3,4,5,6] if it was rotated 6 times.
+
+        Notice that rotating the array 4 times moves the last four elements of the array to the beginning. Rotating the array 6 times produces the original array.
+
+        Assuming all elements in the rotated sorted array nums are unique, return the minimum element of this array.
+
+        A solution that runs in O(n) time is trivial, can you write an algorithm that runs in O(log n) time?
+
+        Example 1:
+
+        Input: nums = [3,4,5,6,1,2]
+
+        Output: 1
+
+        Example 2:
+
+        Input: nums = [4,5,0,1,2,3]
+
+        Output: 0
+
+        Example 3:
+
+        Input: nums = [4,5,6,7]
+
+        Output: 4
+
+        Constraints:
+
+            1 <= nums.length <= 1000
+            -1000 <= nums[i] <= 1000
+
         Extra Test Case:
         [3,1,2] 58 / 150 testcases
         [2,1] 108 / 150 testcases
         https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
         */
+
+        /*
+        Complete explanation
+        
+        At each step, check if the middle element is greater than the rightmost element.
+        If so, the minimum must be to the right.
+        Otherwise, the minimum is at mid or to the left.
+        */
         public int FindMin(int[] nums)
         {
-            int l = 0, r = nums.Length - 1;
-            int res = nums[0];
+            int left = 0;
+            int right = nums.Length - 1;
 
-            while (l <= r)
+            while (left < right)
             {
-                if (nums[l] < nums[r])
-                {
-                    res = Math.Min(res, nums[l]);
-                    break;
-                }
+                int mid = left + (right - left) / 2;
 
-                int m = l + (r - l) / 2;
-                res = Math.Min(res, nums[m]);
-                if (nums[m] >= nums[l])
+                //If mid element is greater than the rightmost,min is in right half
+                if (nums[mid] > nums[right])
                 {
-                    l = m + 1;
+                    left = mid + 1;
                 }
                 else
                 {
-                    r = m - 1;
+                    //Min is at the mid or to the left
+                    right = mid;
                 }
             }
-            return res;
+            //left == right is the index of the minimum
+            return nums[left];
         }
         #endregion
         #region Search in Rotated Sorted Array
