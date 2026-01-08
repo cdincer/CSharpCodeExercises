136452fdfadb8b5c61e75f3446dc98bd37dd8534
cdincer <dincercan@gmail.com>
2026-01-08 04:32:29 +0300

    Search in Rotated Sorted Array required small refactor

--- Neetcode150/BinarySearchNeet.cs
+++ Neetcode150/BinarySearchNeet.cs
@@ -317,7 +317,7 @@ namespace Neetcode150
 
             while (left <= right)
             {
-                int mid = left + (right - left) / 2;
+                int mid = (right + left) / 2;
 
                 if (nums[mid] == target)
                     return mid;
