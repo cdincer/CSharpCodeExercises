7d8c0985123d80e8ae5314e45c6bf18168d8da09
cdincer <dincercan@gmail.com>
2026-01-14 04:19:21 +0300

    Word Search II solution replaced

--- Neetcode150/TriesNeet.cs
+++ Neetcode150/TriesNeet.cs
@@ -352,73 +352,83 @@ namespace Neetcode150
         //Neetcode answer to this problem times out at 64th test case.
         //Solution below doesn't.
 
-        public class Node
+        class TrieNode3
         {
-            public Node[] Next { get; } = new Node['z' + 1];
-            public string Word { get; set; }
-        }
-        public IList<string> FindWords(char[][] board, string[] words)
-        {
-            int rl = board.Length;
-            int cl = board[0].Length;
-            List<string> res = new();
+            public TrieNode3[] children = new TrieNode3[26];
+            public int idx = -1;
+            public int refs = 0;
 
-            //build trie
-            Node root = new();
-            foreach (string word in words)
+            public void AddWord(string word, int i)
             {
-                Node node = root;
+                TrieNode3 cur = this;
+                cur.refs++;
                 foreach (char c in word)
-                {  
-                    if (node.Next[c] is null)
-                        node.Next[c] = new Node();
-                    
-                    node = node.Next[c];
+                {
+                    int index = c - 'a';
+                    if (cur.children[index] == null)
+                    {
+                        cur.children[index] = new TrieNode3();
+                    }
+                    cur = cur.children[index];
+                    cur.refs++;
                 }
-                node.Word = word;
+                cur.idx = i;
+            }
+        }
+        private List<string> res = new List<string>();
+
+        public List<string> FindWords(char[][] board, string[] words)
+        {
+            TrieNode3 root = new TrieNode3();
+            for (int i = 0; i < words.Length; i++)
+            {
+                root.AddWord(words[i], i);
             }
 
-            //do dfs
-            for (int r = 0; r < rl; r++)
+            for (int r = 0; r < board.Length; r++)
             {
-                for (int c = 0; c < cl; c++)
+                for (int c = 0; c < board[0].Length; c++)
                 {
-                    Dfs(r, c, root);
+                    Dfs(board, root, r, c, words);
                 }
             }
 
             return res;
+        }
 
-            void Dfs(int row, int col, Node node)
+        private void Dfs(char[][] board, TrieNode3 node, int r, int c, string[] words)
+        {
+            if (r < 0 || c < 0 || r >= board.Length ||
+                c >= board[0].Length || board[r][c] == '*' ||
+                node.children[board[r][c] - 'a'] == null)
             {
-                if (row < 0 || col < 0 || row == rl || col == cl) return;
-
-                char c = board[row][col];
-
-                if (c == '/' || node.Next[c] == null) return;
-                
-                node = node.Next[c];
-
-                if (node.Word != null)
-                {
-
-                    res.Add(node.Word);
-                    node.Word = null;
-
-                    if (res.Count == words.Length)
-
-                    {
-                        return;
-                    }
-                }
-
-                board[row][col] = '/';
-                Dfs(row - 1, col, node);
-                Dfs(row + 1, col, node);
-                Dfs(row, col + 1, node);
-                Dfs(row, col - 1, node);
-                board[row][col] = c;
+                return;
             }
+
+            char temp = board[r][c];
+            board[r][c] = '*';
+            TrieNode3 prev = node;
+            node = node.children[temp - 'a'];
+            if (node.idx != -1)
+            {
+                res.Add(words[node.idx]);
+                node.idx = -1;
+                node.refs--;
+                if (node.refs == 0)
+                {
+                    node = null;
+                    prev.children[temp - 'a'] = null;
+                    board[r][c] = temp;
+                    return;
+                }
+            }
+
+            Dfs(board, node, r + 1, c, words);
+            Dfs(board, node, r - 1, c, words);
+            Dfs(board, node, r, c + 1, words);
+            Dfs(board, node, r, c - 1, words);
+
+            board[r][c] = temp;
         }
 
         #endregion
