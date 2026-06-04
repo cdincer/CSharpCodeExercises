6c0493ccbce6dcbe94c2ed0cd69039f4679575ac
cdincer <dincercan@gmail.com>
2026-06-04 09:52:04 +0300

    Longest Increasing Path in a Matrix question name fixed

--- Neetcode150/DpTwoNeet.cs
+++ Neetcode150/DpTwoNeet.cs
@@ -425,7 +425,7 @@ namespace Neetcode150
         }
         #endregion Space Optimized 2-D DP
         #endregion
-        #region  Increasing Path in a Matrix
+        #region  Longest Increasing Path in a Matrix
         /*
         Given an m x n integers matrix, return the length of the longest increasing path in matrix.
         From each cell, you can either move in four directions: left, right, up, or down. You may not move diagonally or move outside the boundary (i.e., wrap-around is not allowed).
